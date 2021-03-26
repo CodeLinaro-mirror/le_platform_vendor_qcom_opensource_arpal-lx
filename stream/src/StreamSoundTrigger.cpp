tangalbert919/android_vendor_qcom_opensource_pal

pal: sleep after each read in st stream
Sound Trigger stream read data from ringbuffer inside
pal with almost no delay, making client calling read
with too much times as sometimes data is not enough in
ringbuffer.

Sleep everytime after read pcm data from ring buffer based
on read size.

Change-Id: I1f2f736d2cb851f8a20aaf705f84f284f8c93449

stream/src/StreamSoundTrigger.cpp
@@ -31,6 +31,7 @@
 
 #include "StreamSoundTrigger.h"
 
+#include <chrono>
 #include <unistd.h>
 
 #include "Session.h"
@@ -272,6 +273,7 @@ int32_t StreamSoundTrigger::stop() {
 
 int32_t StreamSoundTrigger::read(struct pal_buffer* buf) {
     int32_t size = 0;
+    uint32_t sleep_ms = 0;
 
     PAL_VERBOSE(LOG_TAG, "Enter");
 
@@ -279,6 +281,17 @@ int32_t StreamSoundTrigger::read(struct pal_buffer* buf) {
     std::shared_ptr<StEventConfig> ev_cfg(
         new StReadBufferEventConfig((void *)buf));
     size = cur_state_->ProcessEvent(ev_cfg);
+
+    /*
+     * st stream read pcm data from ringbuffer with no almost
+     * delay, sleep for some time after each read even if read
+     * fails.
+     */
+
+    sleep_ms = (buf->size * BITS_PER_BYTE * MS_PER_SEC) /
+        (sm_cfg_->GetSampleRate() * sm_cfg_->GetBitWidth() *
+         sm_cfg_->GetOutChannels());
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
 
     PAL_VERBOSE(LOG_TAG, "Exit, read size %d", size);
 
@@ -1383,9 +1396,9 @@ int32_t StreamSoundTrigger::SendRecognitionConfig(
     // create ring buffer for lab transfer in gsl_engine
     ring_buffer_len = hist_buffer_duration + pre_roll_duration +
         client_capture_read_delay;
-    ring_buffer_size = (ring_buffer_len / 1000) * sm_cfg_->GetSampleRate() *
+    ring_buffer_size = (ring_buffer_len / MS_PER_SEC) * sm_cfg_->GetSampleRate() *
                        sm_cfg_->GetBitWidth() *
-                       sm_cfg_->GetOutChannels() / 8;
+                       sm_cfg_->GetOutChannels() / BITS_PER_BYTE;
     status = gsl_engine_->CreateBuffer(ring_buffer_size,
                                        engines_.size(), reader_list_);
     if (status) {

CodeLinaro-mirror/le_platform_vendor_qcom_opensource_arpal-lx

stream/src/StreamSoundTrigger.cpp
@@ -31,6 +31,7 @@
 
 #include "StreamSoundTrigger.h"
 
+#include <chrono>
 #include <unistd.h>
 
 #include "Session.h"
@@ -264,6 +265,7 @@ int32_t StreamSoundTrigger::stop() {
 
 int32_t StreamSoundTrigger::read(struct pal_buffer* buf) {
     int32_t size = 0;
+    uint32_t sleep_ms = 0;
 
     PAL_VERBOSE(LOG_TAG, "Enter");
 
@@ -271,6 +273,17 @@ int32_t StreamSoundTrigger::read(struct pal_buffer* buf) {
     std::shared_ptr<StEventConfig> ev_cfg(
         new StReadBufferEventConfig((void *)buf));
     size = cur_state_->ProcessEvent(ev_cfg);
+
+    /*
+     * st stream read pcm data from ringbuffer with no almost
+     * delay, sleep for some time after each read even if read
+     * fails.
+     */
+
+    sleep_ms = (buf->size * BITS_PER_BYTE * MS_PER_SEC) /
+        (sm_info_->GetSampleRate() * sm_info_->GetBitWidth() *
+         sm_info_->GetOutChannels());
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
 
     PAL_VERBOSE(LOG_TAG, "Exit, read size %d", size);
 
@@ -1303,9 +1316,9 @@ int32_t StreamSoundTrigger::SendRecognitionConfig(
     // create ring buffer for lab transfer in gsl_engine
     ring_buffer_len = hist_buffer_duration + pre_roll_duration +
         client_capture_read_delay;
-    ring_buffer_size = (ring_buffer_len / 1000) * sm_info_->GetSampleRate() *
+    ring_buffer_size = (ring_buffer_len / MS_PER_SEC) * sm_info_->GetSampleRate() *
                        sm_info_->GetBitWidth() *
-                       sm_info_->GetOutChannels() / 8;
+                       sm_info_->GetOutChannels() / BITS_PER_BYTE;
     status = gsl_engine_->CreateBuffer(ring_buffer_size,
                                        engines_.size(), reader_list);
     if (status) {