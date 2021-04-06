059f63be950dd4e4983d4bcd43a3c6b4cb319ab4
xsang <xsang@codeaurora.org>
2021-04-06 11:18:57 +0800

    PAL: avoid deadlock at device switch during gef setparam
    Deadlock might happen when GEF setparam holds resource manager
    lock and wants to hold stream mutex. If pal_stream_write happens,
    it holds stream mutex and wants to get resource manager lock.
    
    Change-Id: I0dfdf190975661e4d1feeb7b746c63d0e03b0144

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -4945,7 +4945,6 @@ int ResourceManager::getParameter(uint32_t param_id, void *param_payload,
     int status = -EINVAL;
 
     PAL_INFO(LOG_TAG, "param_id=%d", param_id);
-    mResourceManagerMutex.lock();
     switch (param_id) {
         case PAL_PARAM_ID_UIEFFECT:
         {
@@ -4965,8 +4964,6 @@ int ResourceManager::getParameter(uint32_t param_id, void *param_payload,
             PAL_ERR(LOG_TAG, "Unknown ParamID:%d", param_id);
             break;
     }
-
-    mResourceManagerMutex.unlock();
 
     return status;
 }
@@ -5413,7 +5410,6 @@ int ResourceManager::setParameter(uint32_t param_id, void *param_payload,
 
     PAL_DBG(LOG_TAG, "Enter param id: %d", param_id);
 
-    mResourceManagerMutex.lock();
     switch (param_id) {
         case PAL_PARAM_ID_UIEFFECT:
         {
@@ -5438,7 +5434,6 @@ int ResourceManager::setParameter(uint32_t param_id, void *param_payload,
             break;
     }
 
-    mResourceManagerMutex.unlock();
     PAL_DBG(LOG_TAG, "Exit status: %d",status);
     return status;
 }
