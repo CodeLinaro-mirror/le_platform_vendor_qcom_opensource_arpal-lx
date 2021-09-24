tangalbert919/android_vendor_qcom_opensource_pal

pal: GKV customization with usecasemanager xml
Retrieve GKVs from usecasemanager xml file in
payload builder instead of hardcoding the same.

Change-Id: I1f360aafc6ffbd1ea4870efa3c5d385407fde117

PalDefs.h
@@ -87,6 +87,7 @@ typedef enum {
     PAL_AUDIO_FMT_PCM_S24_3LE = 0x15,       /**<24 bit packed little endian PCM*/
     PAL_AUDIO_FMT_PCM_S24_LE = 0x16,        /**<24bit in 32bit word (LSB aligned) little endian PCM*/
     PAL_AUDIO_FMT_PCM_S32_LE = 0x17,        /**< 32bit little endian PCM*/
+    PAL_AUDIO_FMT_NON_PCM = 0xE0000000,     /* Internal Constant used for Non PCM format identification */
     PAL_AUDIO_FMT_COMPRESSED_RANGE_BEGIN = 0xF0000000,  /* Reserved for beginning of compressed codecs */
     PAL_AUDIO_FMT_COMPRESSED_EXTENDED_RANGE_BEGIN   = 0xF0000F00,  /* Reserved for beginning of 3rd party codecs */
     PAL_AUDIO_FMT_COMPRESSED_EXTENDED_RANGE_END     = 0xF0000FFF,  /* Reserved for beginning of 3rd party codecs */
@@ -407,6 +408,27 @@ typedef enum {
     PAL_DEVICE_IN_MAX = PAL_DEVICE_IN_MIN + 21,
 } pal_device_id_t;
 
+typedef enum {
+    VOICEMMODE1 = 0x11C05000,
+    VOICEMMODE2 = 0x11DC5000,
+    VOICELBMMODE1 = 0x12006000,
+    VOICELBMMODE2 = 0x121C6000,
+} pal_VSID_t;
+
+typedef enum {
+    PAL_STREAM_LOOPBACK_PCM,
+    PAL_STREAM_LOOPBACK_HFP_RX,
+    PAL_STREAM_LOOPBACK_HFP_TX,
+    PAL_STREAM_LOOPBACK_COMPRESS,
+    PAL_STREAM_LOOPBACK_FM,
+} pal_stream_loopback_type_t;
+
+typedef enum {
+    PAL_STREAM_PROXY_TX_VISUALIZER,
+    PAL_STREAM_PROXY_TX_WFD,
+    PAL_STREAM_PROXY_TX_TELEPHONY_RX,
+} pal_stream_proxy_tx_type_t;
+
 #ifdef __cplusplus
 static const std::map<std::string, pal_device_id_t> deviceIdLUT {
     {std::string{ "PAL_DEVICE_OUT_MIN" },                  PAL_DEVICE_OUT_MIN},
@@ -498,6 +520,79 @@ static const std::map<uint32_t, std::string> deviceNameLUT {
     {PAL_DEVICE_IN_ULTRASOUND_MIC,        std::string{"PAL_DEVICE_IN_ULTRASOUND_MIC"}},
     {PAL_DEVICE_IN_EXT_EC_REF,            std::string{"PAL_DEVICE_IN_EXT_EC_REF"}}
 };
+
+const std::map<std::string, uint32_t> usecaseIdLUT {
+    {std::string{ "PAL_STREAM_LOW_LATENCY" },              PAL_STREAM_LOW_LATENCY},
+    {std::string{ "PAL_STREAM_DEEP_BUFFER" },              PAL_STREAM_DEEP_BUFFER},
+    {std::string{ "PAL_STREAM_COMPRESSED" },               PAL_STREAM_COMPRESSED},
+    {std::string{ "PAL_STREAM_VOIP" },                     PAL_STREAM_VOIP},
+    {std::string{ "PAL_STREAM_VOIP_RX" },                  PAL_STREAM_VOIP_RX},
+    {std::string{ "PAL_STREAM_VOIP_TX" },                  PAL_STREAM_VOIP_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL_MUSIC" },         PAL_STREAM_VOICE_CALL_MUSIC},
+    {std::string{ "PAL_STREAM_GENERIC" },                  PAL_STREAM_GENERIC},
+    {std::string{ "PAL_STREAM_RAW" },                      PAL_STREAM_RAW},
+    {std::string{ "PAL_STREAM_VOICE_ACTIVATION" },         PAL_STREAM_VOICE_ACTIVATION},
+    {std::string{ "PAL_STREAM_VOICE_CALL_RECORD" },        PAL_STREAM_VOICE_CALL_RECORD},
+    {std::string{ "PAL_STREAM_VOICE_CALL_TX" },            PAL_STREAM_VOICE_CALL_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL_RX_TX" },         PAL_STREAM_VOICE_CALL_RX_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL" },               PAL_STREAM_VOICE_CALL},
+    {std::string{ "PAL_STREAM_LOOPBACK" },                 PAL_STREAM_LOOPBACK},
+    {std::string{ "PAL_STREAM_TRANSCODE" },                PAL_STREAM_TRANSCODE},
+    {std::string{ "PAL_STREAM_VOICE_UI" },                 PAL_STREAM_VOICE_UI},
+    {std::string{ "PAL_STREAM_PCM_OFFLOAD" },              PAL_STREAM_PCM_OFFLOAD},
+    {std::string{ "PAL_STREAM_ULTRA_LOW_LATENCY" },        PAL_STREAM_ULTRA_LOW_LATENCY},
+    {std::string{ "PAL_STREAM_PROXY" },                    PAL_STREAM_PROXY},
+    {std::string{ "PAL_STREAM_NON_TUNNEL" },               PAL_STREAM_NON_TUNNEL},
+    {std::string{ "PAL_STREAM_HAPTICS" },                  PAL_STREAM_HAPTICS},
+    {std::string{ "PAL_STREAM_ACD" },                      PAL_STREAM_ACD},
+    {std::string{ "PAL_STREAM_ULTRASOUND" },               PAL_STREAM_ULTRASOUND},
+    {std::string{ "PAL_STREAM_SENSOR_PCM_DATA" },          PAL_STREAM_SENSOR_PCM_DATA},
+};
+
+/* Update the reverse mapping as well when new stream is added */
+const std::map<uint32_t, std::string> streamNameLUT {
+    {PAL_STREAM_LOW_LATENCY,        std::string{ "PAL_STREAM_LOW_LATENCY" } },
+    {PAL_STREAM_DEEP_BUFFER,        std::string{ "PAL_STREAM_DEEP_BUFFER" } },
+    {PAL_STREAM_COMPRESSED,         std::string{ "PAL_STREAM_COMPRESSED" } },
+    {PAL_STREAM_VOIP,               std::string{ "PAL_STREAM_VOIP" } },
+    {PAL_STREAM_VOIP_RX,            std::string{ "PAL_STREAM_VOIP_RX" } },
+    {PAL_STREAM_VOIP_TX,            std::string{ "PAL_STREAM_VOIP_TX" } },
+    {PAL_STREAM_VOICE_CALL_MUSIC,   std::string{ "PAL_STREAM_VOICE_CALL_MUSIC" } },
+    {PAL_STREAM_GENERIC,            std::string{ "PAL_STREAM_GENERIC" } },
+    {PAL_STREAM_RAW,                std::string{ "PAL_STREAM_RAW" } },
+    {PAL_STREAM_VOICE_ACTIVATION,   std::string{ "PAL_STREAM_VOICE_ACTIVATION" } },
+    {PAL_STREAM_VOICE_CALL_RECORD,  std::string{ "PAL_STREAM_VOICE_CALL_RECORD" } },
+    {PAL_STREAM_VOICE_CALL_TX,      std::string{ "PAL_STREAM_VOICE_CALL_TX" } },
+    {PAL_STREAM_VOICE_CALL_RX_TX,   std::string{ "PAL_STREAM_VOICE_CALL_RX_TX" } },
+    {PAL_STREAM_VOICE_CALL,         std::string{ "PAL_STREAM_VOICE_CALL" } },
+    {PAL_STREAM_LOOPBACK,           std::string{ "PAL_STREAM_LOOPBACK" } },
+    {PAL_STREAM_TRANSCODE,          std::string{ "PAL_STREAM_TRANSCODE" } },
+    {PAL_STREAM_VOICE_UI,           std::string{ "PAL_STREAM_VOICE_UI" } },
+    {PAL_STREAM_PCM_OFFLOAD,        std::string{ "PAL_STREAM_PCM_OFFLOAD" } },
+    {PAL_STREAM_ULTRA_LOW_LATENCY,  std::string{ "PAL_STREAM_ULTRA_LOW_LATENCY" } },
+    {PAL_STREAM_PROXY,              std::string{ "PAL_STREAM_PROXY" } },
+    {PAL_STREAM_NON_TUNNEL,         std::string{ "PAL_STREAM_NON_TUNNEL" } },
+    {PAL_STREAM_HAPTICS,            std::string{ "PAL_STREAM_HAPTICS" } },
+    {PAL_STREAM_ACD,                std::string{ "PAL_STREAM_ACD" } },
+    {PAL_STREAM_ULTRASOUND,         std::string{ "PAL_STREAM_ULTRASOUND" } },
+    {PAL_STREAM_SENSOR_PCM_DATA,    std::string{ "PAL_STREAM_SENSOR_PCM_DATA" } },
+};
+
+const std::map<uint32_t, std::string> vsidLUT {
+    {VOICEMMODE1,    std::string{ "VOICEMMODE1" } },
+    {VOICEMMODE2,    std::string{ "VOICEMMODE2" } },
+    {VOICELBMMODE1,  std::string{ "VOICELBMMODE1" } },
+    {VOICELBMMODE2,  std::string{ "VOICELBMMODE2" } },
+};
+
+const std::map<uint32_t, std::string> loopbackLUT {
+    {PAL_STREAM_LOOPBACK_PCM,        std::string{ "PAL_STREAM_LOOPBACK_PCM" } },
+    {PAL_STREAM_LOOPBACK_HFP_RX,     std::string{ "PAL_STREAM_LOOPBACK_HFP_RX" } },
+    {PAL_STREAM_LOOPBACK_HFP_TX,     std::string{ "PAL_STREAM_LOOPBACK_HFP_TX" } },
+    {PAL_STREAM_LOOPBACK_COMPRESS,   std::string{ "PAL_STREAM_LOOPBACK_COMPRESS" } },
+    {PAL_STREAM_LOOPBACK_FM,         std::string{ "PAL_STREAM_LOOPBACK_FM" } },
+};
+
 #endif
 
 
@@ -514,20 +609,6 @@ typedef enum {
 typedef enum {
     PAL_SND_CARD_STATE,
 } pal_global_callback_event_t;
-
-typedef enum {
-    PAL_STREAM_LOOPBACK_PCM,
-    PAL_STREAM_LOOPBACK_HFP_RX,
-    PAL_STREAM_LOOPBACK_HFP_TX,
-    PAL_STREAM_LOOPBACK_COMPRESS,
-    PAL_STREAM_LOOPBACK_FM
-} pal_stream_loopback_type_t;
-
-typedef enum {
-    PAL_STREAM_PROXY_TX_VISUALIZER,
-    PAL_STREAM_PROXY_TX_WFD,
-    PAL_STREAM_PROXY_TX_TELEPHONY_RX,
-} pal_stream_proxy_tx_type_t;
 
 struct pal_stream_info {
     int64_t version;                    /** version of structure*/
@@ -557,13 +638,6 @@ struct pal_voice_call_info {
      uint32_t VSID;
      uint32_t tty_mode;
 };
-
-typedef enum {
-    VOICEMMODE1 = 0x11C05000,
-    VOICEMMODE2 = 0x11DC5000,
-    VOICELBMMODE1 = 0x12006000,
-    VOICELBMMODE2 = 0x121C6000,
-} pal_VSID_t;
 
 typedef enum {
     PAL_TTY_OFF = 0,

session/inc/PayloadBuilder.h
@@ -38,6 +38,8 @@
 #include <algorithm>
 #include <expat.h>
 #include <map>
+#include <regex>
+#include <sstream>
 #include "Stream.h"
 #include "Device.h"
 #include "ResourceManager.h"
@@ -90,10 +92,85 @@ struct dpAudioConfig{
   uint32_t dptx_idx;
 };
 
+typedef enum {
+    DIRECTION_SEL = 1,
+    BITWIDTH_SEL,
+    INSTANCE_SEL,
+    SUB_TYPE_SEL,
+    VSID_SEL,
+    VUI_MODULE_TYPE_SEL,
+    ACD_MODULE_TYPE_SEL,
+    STREAM_TYPE_SEL,
+    CODECFORMAT_SEL,
+    ABR_ENABLED_SEL,
+    AUD_FMT_SEL,
+    DEVICEPP_TYPE_SEL,
+    CUSTOM_CONFIG_SEL,
+    HOSTLESS_SEL,
+    SIDETONE_MODE_SEL,
+} selector_type_t;
+
+const std::map<std::string, selector_type_t> selectorstypeLUT {
+    {std::string{ "Direction" },             DIRECTION_SEL},
+    {std::string{ "BitWidth" },              BITWIDTH_SEL},
+    {std::string{ "Instance" },              INSTANCE_SEL},
+    {std::string{ "SubType" },               SUB_TYPE_SEL},
+    {std::string{ "VSID" },                  VSID_SEL},
+    {std::string{ "VUIModuleType" },         VUI_MODULE_TYPE_SEL},
+    {std::string{ "ACDModuleType" },         ACD_MODULE_TYPE_SEL},
+    {std::string{ "StreamType" },            STREAM_TYPE_SEL},
+    {std::string{ "DevicePPType" },          DEVICEPP_TYPE_SEL},
+    {std::string{ "CodecFormat" },           CODECFORMAT_SEL},
+    {std::string{ "AbrEnabled" },            ABR_ENABLED_SEL},
+    {std::string{ "AudioFormat" },           AUD_FMT_SEL},
+    {std::string{ "CustomConfig" },          CUSTOM_CONFIG_SEL},
+    {std::string{ "Hostless" },              HOSTLESS_SEL},
+    {std::string{ "SidetoneMode" },          SIDETONE_MODE_SEL},
+};
+
+struct kvPairs {
+    unsigned int key;
+    unsigned int value;
+};
+
+struct kvInfo {
+    std::vector<std::string> selector_names;
+    std::vector<std::pair<selector_type_t, std::string>> selector_pairs;
+    std::vector<kvPairs> kv_pairs;
+};
+
+struct allKVs {
+    std::vector<int> id_type;
+    std::vector<kvInfo> keys_values;
+};
+
+typedef enum {
+    TAG_USECASEXML_ROOT,
+    TAG_STREAM_SEL,
+    TAG_STREAMPP_SEL,
+    TAG_DEVICE_SEL,
+    TAG_DEVICEPP_SEL,
+} usecase_xml_tag;
+
+struct user_xml_data{
+    char data_buf[1024];
+    size_t offs;
+    usecase_xml_tag tag;
+    bool is_parsing_streams;
+    bool is_parsing_streampps;
+    bool is_parsing_devices;
+    bool is_parsing_devicepps;
+};
 class SessionGsl;
 
 class PayloadBuilder
 {
+protected:
+   static std::vector<allKVs> all_streams;
+   static std::vector<allKVs> all_streampps;
+   static std::vector<allKVs> all_devices;
+   static std::vector<allKVs> all_devicepps;
+
 public:
     void payloadUsbAudioConfig(uint8_t** payload, size_t* size,
                            uint32_t miid,
@@ -154,8 +231,30 @@ public:
     int populateTagKeyVector(Stream *s, std::vector <std::pair<int,int>> &tkv, int tag, uint32_t* gsltag);
     void payloadTimestamp(uint8_t **payload, size_t *size, uint32_t moduleId);
     static int init();
-    static void endTag(void *userdata __unused, const XML_Char *tag_name);
-    static void startTag(void *userdata __unused, const XML_Char *tag_name, const XML_Char **attr);
+    static void endTag(void *userdata, const XML_Char *tag_name);
+    static void startTag(void *userdata, const XML_Char *tag_name, const XML_Char **attr);
+    static void handleData(void *userdata, const char *s, int len);
+    static void resetDataBuf(struct user_xml_data *data);
+    static void processKVTypeData(struct user_xml_data *data, const XML_Char **attr);
+    static void processKVSelectorData(struct user_xml_data *data, const XML_Char **attr);
+    static void processGraphKVData(struct user_xml_data *data, const XML_Char **attr);
+    static bool isIdTypeAvailable(int32_t type, std::vector<int32_t>& id_type);
+    static void removeDuplicateSelectors(std::vector<std::string> &gkv_selectors);
+    static std::vector <std::string> retrieveSelectors(int32_t type,
+        std::vector<allKVs> any_type);
+    static std::vector <std::pair<selector_type_t, std::string>> getSelectorValues(
+        std::vector<std::string> &selectors, Stream* s);
+    static bool compareSelectorPairs(std::vector <std::pair<selector_type_t, std::string>>
+        &selector_val, std::vector<std::pair<selector_type_t, std::string>> &filled_selector_pairs);
+    static int retrieveKVs(std::vector<std::pair<selector_type_t, std::string>>
+        &filled_selector_pairs, uint32_t type, std::vector<allKVs> any_type,
+        std::vector<std::pair<int32_t, int32_t>> &keyVector);
+    static std::string removeSpaces(const std::string& str);
+    static std::vector<std::string> splitStrings(const std::string& str);
+    static int getBtDeviceKV(int dev_id, std::vector<std::pair<int, int>> &deviceKV,
+        uint32_t codecFormat, bool isAbrEnabled, bool isHostless);
+    static int getDeviceKV(int dev_id, std::vector<std::pair<int, int>> &deviceKV);
+    static bool isBtA2DPDevice(int32_t beDevId);
     PayloadBuilder();
     ~PayloadBuilder();
 };

CodeLinaro-mirror/le_platform_vendor_qcom_opensource_arpal-lx

pal: Add fallback mechanism for custom config as selector
When a custom config is set and it doesn't need GKV pair
different than the default one, current implementation requires
it to be added in usecase manager XML to pick the GKVs.
Add a fallback mechanism to find the KVs by removing the
custom config as selector when the KV pairs are not
found with it.
Modify the way to get selector values for custom config using
device attributes.
Change-Id: I66c11b1ac92c04f65b3e6ee57e5b838f95f7402d
pal: Update the device derivation logic in device and devicepp KV
In order to populate device and devicepp KVs, the associated devices
used to be obtained from stream. Instead derive the device using
getInstance from device class to avoid the stream dependency as
in some cases like external ec reference devices, stream can be NULL.
Change-Id: Ibc2a7a26ba3b078f6de8808ac2ac58d5236e1361
pal: Sort the key value tags in usecase manager XML
When there are no selector values or filled selector pairs,
the default KV pair which has no selectors should be picked.
But it currently picks the first tag KV pairs. Sort the key value
tags as per number of selectors to fix this.
Change-Id: I1a1a9e7992abc0484597d381656208ce2746f1aa
pal: Clean up the KV pair handling
Remove the KV pair handling logic in resource
manager as it is moved to usecase manager in
payload builder.
Change-Id: Id38c074cc3675f63314cbbcdaf53256078ee5162
temp: fix compilation for gkv
Change-Id: I8a0264acc92c6767613a2997da518bd0507d0375

PalDefs.h
@@ -80,6 +80,13 @@ typedef enum {
     PAL_AUDIO_FMT_PCM_S24_3LE = 0xF,           /**<24 bit packed little endian PCM*/
     PAL_AUDIO_FMT_PCM_S24_LE = 0x10,           /**<24bit in 32bit word (LSB aligned) little endian PCM*/
     PAL_AUDIO_FMT_PCM_S32_LE = 0x11,           /**< 32bit little endian PCM*/
+    PAL_AUDIO_FMT_AMR_NB = 0x12,
+    PAL_AUDIO_FMT_AMR_WB = 0x13,
+    PAL_AUDIO_FMT_AMR_WB_PLUS = 0x14,
+    PAL_AUDIO_FMT_EVRC = 0x15,
+    PAL_AUDIO_FMT_G711 = 0x16,
+    PAL_AUDIO_FMT_QCELP = 0x17,
+    PAL_AUDIO_FMT_NON_PCM = 0xE0000000,     /* Internal Constant used for Non PCM format identification */
     PAL_AUDIO_FMT_COMPRESSED_RANGE_BEGIN = 0xF0000000,  /* Reserved for beginning of compressed codecs */
     PAL_AUDIO_FMT_COMPRESSED_EXTENDED_RANGE_BEGIN   = 0xF0000F00,  /* Reserved for beginning of 3rd party codecs */
     PAL_AUDIO_FMT_COMPRESSED_EXTENDED_RANGE_END     = 0xF0000FFF,  /* Reserved for beginning of 3rd party codecs */
@@ -383,6 +390,30 @@ typedef enum {
     PAL_DEVICE_IN_MAX = PAL_DEVICE_IN_MIN + 19,
 } pal_device_id_t;
 
+typedef enum {
+    VOICEMMODE1 = 0x11C05000,
+    VOICEMMODE2 = 0x11DC5000,
+    VOICELBMMODE1 = 0x12006000,
+    VOICELBMMODE2 = 0x121C6000,
+} pal_VSID_t;
+
+
+typedef enum {
+    PAL_STREAM_PROXY_TX_VISUALIZER,
+    PAL_STREAM_PROXY_TX_WFD,
+    PAL_STREAM_PROXY_TX_TELEPHONY_RX,
+} pal_stream_proxy_tx_type_t;
+
+typedef enum {
+    PAL_STREAM_LOOPBACK_PCM,
+    PAL_STREAM_LOOPBACK_HFP_RX,
+    PAL_STREAM_LOOPBACK_HFP_TX,
+    PAL_STREAM_LOOPBACK_COMPRESS,
+    PAL_STREAM_LOOPBACK_FM,
+    PAL_STREAM_LOOPBACK_PLAYBACK_ONLY,
+    PAL_STREAM_LOOPBACK_CAPTURE_ONLY
+} pal_stream_loopback_type_t;
+
 #ifdef __cplusplus
 static const std::map<std::string, pal_device_id_t> deviceIdLUT {
     {std::string{ "PAL_DEVICE_OUT_MIN" },                  PAL_DEVICE_OUT_MIN},
@@ -464,6 +495,81 @@ static const std::map<uint32_t, std::string> deviceNameLUT {
     {PAL_DEVICE_IN_VI_FEEDBACK,           std::string{"PAL_DEVICE_IN_VI_FEEDBACK"}},
     {PAL_DEVICE_IN_EXT_EC_REF,            std::string{"PAL_DEVICE_IN_EXT_EC_REF"}},
 };
+
+const std::map<std::string, uint32_t> usecaseIdLUT {
+    {std::string{ "PAL_STREAM_LOW_LATENCY" },              PAL_STREAM_LOW_LATENCY},
+    {std::string{ "PAL_STREAM_DEEP_BUFFER" },              PAL_STREAM_DEEP_BUFFER},
+    {std::string{ "PAL_STREAM_COMPRESSED" },               PAL_STREAM_COMPRESSED},
+    {std::string{ "PAL_STREAM_VOIP" },                     PAL_STREAM_VOIP},
+    {std::string{ "PAL_STREAM_VOIP_RX" },                  PAL_STREAM_VOIP_RX},
+    {std::string{ "PAL_STREAM_VOIP_TX" },                  PAL_STREAM_VOIP_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL_MUSIC" },         PAL_STREAM_VOICE_CALL_MUSIC},
+    {std::string{ "PAL_STREAM_GENERIC" },                  PAL_STREAM_GENERIC},
+    {std::string{ "PAL_STREAM_RAW" },                      PAL_STREAM_RAW},
+    {std::string{ "PAL_STREAM_VOICE_ACTIVATION" },         PAL_STREAM_VOICE_ACTIVATION},
+    {std::string{ "PAL_STREAM_VOICE_CALL_RECORD" },        PAL_STREAM_VOICE_CALL_RECORD},
+    {std::string{ "PAL_STREAM_VOICE_CALL_TX" },            PAL_STREAM_VOICE_CALL_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL_RX_TX" },         PAL_STREAM_VOICE_CALL_RX_TX},
+    {std::string{ "PAL_STREAM_VOICE_CALL" },               PAL_STREAM_VOICE_CALL},
+    {std::string{ "PAL_STREAM_LOOPBACK" },                 PAL_STREAM_LOOPBACK},
+    {std::string{ "PAL_STREAM_TRANSCODE" },                PAL_STREAM_TRANSCODE},
+    {std::string{ "PAL_STREAM_VOICE_UI" },                 PAL_STREAM_VOICE_UI},
+    {std::string{ "PAL_STREAM_PCM_OFFLOAD" },              PAL_STREAM_PCM_OFFLOAD},
+    {std::string{ "PAL_STREAM_ULTRA_LOW_LATENCY" },        PAL_STREAM_ULTRA_LOW_LATENCY},
+    {std::string{ "PAL_STREAM_PROXY" },                    PAL_STREAM_PROXY},
+    {std::string{ "PAL_STREAM_PLAYBACK_MEDIA" },           PAL_STREAM_PLAYBACK_MEDIA},
+    {std::string{ "PAL_STREAM_PLAYBACK_SYS_NOTIFICATION" },PAL_STREAM_PLAYBACK_SYS_NOTIFICATION},
+    {std::string{ "PAL_STREAM_PLAYBACK_NAV_GUIDANCE" },    PAL_STREAM_PLAYBACK_NAV_GUIDANCE},
+    {std::string{ "PAL_STREAM_PLAYBACK_PHONE" },           PAL_STREAM_PLAYBACK_PHONE},
+    {std::string{ "PAL_STREAM_PLAYBACK_FRONT_PASSENGER" }, PAL_STREAM_PLAYBACK_FRONT_PASSENGER},
+    {std::string{ "PAL_STREAM_PLAYBACK_REAR_SEAT" },       PAL_STREAM_PLAYBACK_REAR_SEAT},
+};
+
+/* Update the reverse mapping as well when new stream is added */
+const std::map<uint32_t, std::string> streamNameLUT {
+    {PAL_STREAM_LOW_LATENCY,                 std::string{ "PAL_STREAM_LOW_LATENCY" } },
+    {PAL_STREAM_DEEP_BUFFER,                 std::string{ "PAL_STREAM_DEEP_BUFFER" } },
+    {PAL_STREAM_COMPRESSED,                  std::string{ "PAL_STREAM_COMPRESSED" } },
+    {PAL_STREAM_VOIP,                        std::string{ "PAL_STREAM_VOIP" } },
+    {PAL_STREAM_VOIP_RX,                     std::string{ "PAL_STREAM_VOIP_RX" } },
+    {PAL_STREAM_VOIP_TX,                     std::string{ "PAL_STREAM_VOIP_TX" } },
+    {PAL_STREAM_VOICE_CALL_MUSIC,            std::string{ "PAL_STREAM_VOICE_CALL_MUSIC" } },
+    {PAL_STREAM_GENERIC,                     std::string{ "PAL_STREAM_GENERIC" } },
+    {PAL_STREAM_RAW,                         std::string{ "PAL_STREAM_RAW" } },
+    {PAL_STREAM_VOICE_ACTIVATION,            std::string{ "PAL_STREAM_VOICE_ACTIVATION" } },
+    {PAL_STREAM_VOICE_CALL_RECORD,           std::string{ "PAL_STREAM_VOICE_CALL_RECORD" } },
+    {PAL_STREAM_VOICE_CALL_TX,               std::string{ "PAL_STREAM_VOICE_CALL_TX" } },
+    {PAL_STREAM_VOICE_CALL_RX_TX,            std::string{ "PAL_STREAM_VOICE_CALL_RX_TX" } },
+    {PAL_STREAM_VOICE_CALL,                  std::string{ "PAL_STREAM_VOICE_CALL" } },
+    {PAL_STREAM_LOOPBACK,                    std::string{ "PAL_STREAM_LOOPBACK" } },
+    {PAL_STREAM_TRANSCODE,                   std::string{ "PAL_STREAM_TRANSCODE" } },
+    {PAL_STREAM_VOICE_UI,                    std::string{ "PAL_STREAM_VOICE_UI" } },
+    {PAL_STREAM_PCM_OFFLOAD,                 std::string{ "PAL_STREAM_PCM_OFFLOAD" } },
+    {PAL_STREAM_ULTRA_LOW_LATENCY,           std::string{ "PAL_STREAM_ULTRA_LOW_LATENCY" } },
+    {PAL_STREAM_PROXY,                       std::string{ "PAL_STREAM_PROXY" } },
+    {PAL_STREAM_PLAYBACK_MEDIA,              std::string{ "PAL_STREAM_PLAYBACK_MEDIA" } },
+    {PAL_STREAM_PLAYBACK_SYS_NOTIFICATION,   std::string{ "PAL_STREAM_PLAYBACK_SYS_NOTIFICATION" } },
+    {PAL_STREAM_PLAYBACK_NAV_GUIDANCE,       std::string{ "PAL_STREAM_PLAYBACK_NAV_GUIDANCE" } },
+    {PAL_STREAM_PLAYBACK_PHONE,              std::string{ "PAL_STREAM_PLAYBACK_PHONE" } },
+    {PAL_STREAM_PLAYBACK_FRONT_PASSENGER,    std::string{ "PAL_STREAM_PLAYBACK_FRONT_PASSENGER" } },
+    {PAL_STREAM_PLAYBACK_REAR_SEAT,          std::string{ "PAL_STREAM_PLAYBACK_REAR_SEAT" } },
+};
+
+const std::map<uint32_t, std::string> vsidLUT {
+    {VOICEMMODE1,    std::string{ "VOICEMMODE1" } },
+    {VOICEMMODE2,    std::string{ "VOICEMMODE2" } },
+    {VOICELBMMODE1,  std::string{ "VOICELBMMODE1" } },
+    {VOICELBMMODE2,  std::string{ "VOICELBMMODE2" } },
+};
+
+const std::map<uint32_t, std::string> loopbackLUT {
+    {PAL_STREAM_LOOPBACK_PCM,        std::string{ "PAL_STREAM_LOOPBACK_PCM" } },
+    {PAL_STREAM_LOOPBACK_HFP_RX,     std::string{ "PAL_STREAM_LOOPBACK_HFP_RX" } },
+    {PAL_STREAM_LOOPBACK_HFP_TX,     std::string{ "PAL_STREAM_LOOPBACK_HFP_TX" } },
+    {PAL_STREAM_LOOPBACK_COMPRESS,   std::string{ "PAL_STREAM_LOOPBACK_COMPRESS" } },
+    {PAL_STREAM_LOOPBACK_FM,         std::string{ "PAL_STREAM_LOOPBACK_FM" } },
+};
+
 #endif
 
 
@@ -480,20 +586,6 @@ typedef enum {
     PAL_SND_CARD_STATE,
 } pal_global_callback_event_t;
 
-typedef enum {
-    PAL_STREAM_LOOPBACK_PCM,
-    PAL_STREAM_LOOPBACK_HFP_RX,
-    PAL_STREAM_LOOPBACK_HFP_TX,
-    PAL_STREAM_LOOPBACK_COMPRESS,
-    PAL_STREAM_LOOPBACK_FM,
-    PAL_STREAM_LOOPBACK_PLAYBACK_ONLY,
-    PAL_STREAM_LOOPBACK_CAPTURE_ONLY
-} pal_stream_loopback_type_t;
-
-typedef enum {
-    PAL_STREAM_PROXY_TX_VISUALIZER,
-    PAL_STREAM_PROXY_TX_WFD,
-} pal_stream_proxy_tx_type_t;
 
 struct pal_stream_info {
     int64_t version;                    /** version of structure*/
@@ -522,13 +614,6 @@ struct pal_voice_call_info {
      uint32_t VSID;
      uint32_t tty_mode;
 };
-
-typedef enum {
-    VOICEMMODE1 = 0x11C05000,
-    VOICEMMODE2 = 0x11DC5000,
-    VOICELBMMODE1 = 0x12006000,
-    VOICELBMMODE2 = 0x121C6000,
-}pal_VSID_t;
 
 typedef enum {
     PAL_TTY_OFF = 0,

session/inc/PayloadBuilder.h
@@ -38,6 +38,8 @@
 #include <algorithm>
 #include <expat.h>
 #include <map>
+#include <regex>
+#include <sstream>
 #include "Stream.h"
 #include "Device.h"
 #include "ResourceManager.h"
@@ -90,10 +92,85 @@ struct dpAudioConfig{
   uint32_t dptx_idx;
 };
 
+typedef enum {
+    DIRECTION_SEL = 1,
+    BITWIDTH_SEL,
+    INSTANCE_SEL,
+    SUB_TYPE_SEL,
+    VSID_SEL,
+    VUI_MODULE_TYPE_SEL,
+    ACD_MODULE_TYPE_SEL,
+    STREAM_TYPE_SEL,
+    CODECFORMAT_SEL,
+    ABR_ENABLED_SEL,
+    AUD_FMT_SEL,
+    DEVICEPP_TYPE_SEL,
+    CUSTOM_CONFIG_SEL,
+    HOSTLESS_SEL,
+    SIDETONE_MODE_SEL,
+} selector_type_t;
+
+const std::map<std::string, selector_type_t> selectorstypeLUT {
+    {std::string{ "Direction" },             DIRECTION_SEL},
+    {std::string{ "BitWidth" },              BITWIDTH_SEL},
+    {std::string{ "Instance" },              INSTANCE_SEL},
+    {std::string{ "SubType" },               SUB_TYPE_SEL},
+    {std::string{ "VSID" },                  VSID_SEL},
+    {std::string{ "VUIModuleType" },         VUI_MODULE_TYPE_SEL},
+    {std::string{ "ACDModuleType" },         ACD_MODULE_TYPE_SEL},
+    {std::string{ "StreamType" },            STREAM_TYPE_SEL},
+    {std::string{ "DevicePPType" },          DEVICEPP_TYPE_SEL},
+    {std::string{ "CodecFormat" },           CODECFORMAT_SEL},
+    {std::string{ "AbrEnabled" },            ABR_ENABLED_SEL},
+    {std::string{ "AudioFormat" },           AUD_FMT_SEL},
+    {std::string{ "CustomConfig" },          CUSTOM_CONFIG_SEL},
+    {std::string{ "Hostless" },              HOSTLESS_SEL},
+    {std::string{ "SidetoneMode" },          SIDETONE_MODE_SEL},
+};
+
+struct kvPairs {
+    unsigned int key;
+    unsigned int value;
+};
+
+struct kvInfo {
+    std::vector<std::string> selector_names;
+    std::vector<std::pair<selector_type_t, std::string>> selector_pairs;
+    std::vector<kvPairs> kv_pairs;
+};
+
+struct allKVs {
+    std::vector<int> id_type;
+    std::vector<kvInfo> keys_values;
+};
+
+typedef enum {
+    TAG_USECASEXML_ROOT,
+    TAG_STREAM_SEL,
+    TAG_STREAMPP_SEL,
+    TAG_DEVICE_SEL,
+    TAG_DEVICEPP_SEL,
+} usecase_xml_tag;
+
+struct user_xml_data{
+    char data_buf[1024];
+    size_t offs;
+    usecase_xml_tag tag;
+    bool is_parsing_streams;
+    bool is_parsing_streampps;
+    bool is_parsing_devices;
+    bool is_parsing_devicepps;
+};
 class SessionGsl;
 
 class PayloadBuilder
 {
+protected:
+   static std::vector<allKVs> all_streams;
+   static std::vector<allKVs> all_streampps;
+   static std::vector<allKVs> all_devices;
+   static std::vector<allKVs> all_devicepps;
+
 public:
     void payloadUsbAudioConfig(uint8_t** payload, size_t* size,
                            uint32_t miid,
@@ -141,16 +218,41 @@ public:
     int populateDeviceKV(Stream* s, int32_t rxBeDevId, std::vector <std::pair<int,int>> &keyVectorRx,
         int32_t txBeDevId, std::vector <std::pair<int,int>> &keyVectorTx, sidetone_mode_t sidetoneMode);
     int populateDevicePPKV(Stream* s, int32_t rxBeDevId, std::vector <std::pair<int,int>> &keyVectorRx,
-        int32_t txBeDevId, std::vector <std::pair<int,int>> &keyVectorTx,
-        std::vector<kvpair_info> kvpair);
+        int32_t txBeDevId, std::vector <std::pair<int,int>> &keyVectorTx);
     int populateDevicePPCkv(Stream *s, std::vector <std::pair<int,int>> &keyVector);
     int populateStreamCkv(Stream *s, std::vector <std::pair<int,int>> &keyVector, int tag, struct pal_volume_data **);
     int populateCalKeyVector(Stream *s, std::vector <std::pair<int,int>> &ckv, int tag);
     int populateTagKeyVector(Stream *s, std::vector <std::pair<int,int>> &tkv, int tag, uint32_t* gsltag);
     void payloadTimestamp(uint8_t **payload, size_t *size, uint32_t moduleId);
     static int init();
-    static void endTag(void *userdata __unused, const XML_Char *tag_name);
-    static void startTag(void *userdata __unused, const XML_Char *tag_name, const XML_Char **attr);
+    static void endTag(void *userdata, const XML_Char *tag_name);
+    static void startTag(void *userdata, const XML_Char *tag_name, const XML_Char **attr);
+    static void handleData(void *userdata, const char *s, int len);
+    static void resetDataBuf(struct user_xml_data *data);
+    static void processKVTypeData(struct user_xml_data *data, const XML_Char **attr);
+    static void processKVSelectorData(struct user_xml_data *data, const XML_Char **attr);
+    static void processGraphKVData(struct user_xml_data *data, const XML_Char **attr);
+    static bool isIdTypeAvailable(int32_t type, std::vector<int32_t>& id_type);
+    static void removeDuplicateSelectors(std::vector<std::string> &gkv_selectors);
+    static std::vector <std::string> retrieveSelectors(int32_t type,
+        std::vector<allKVs> any_type);
+    static std::vector <std::pair<selector_type_t, std::string>> getSelectorValues(
+        std::vector<std::string> &selectors, Stream* s, struct pal_device* dAttr);
+    static bool compareSelectorPairs(std::vector <std::pair<selector_type_t, std::string>>
+        &selector_val, std::vector<std::pair<selector_type_t, std::string>> &filled_selector_pairs);
+    static int retrieveKVs(std::vector<std::pair<selector_type_t, std::string>>
+        &filled_selector_pairs, uint32_t type, std::vector<allKVs> any_type,
+        std::vector<std::pair<int32_t, int32_t>> &keyVector);
+    static bool findKVs(std::vector<std::pair<selector_type_t, std::string>>
+        &filled_selector_pairs, uint32_t type, std::vector<allKVs> any_type,
+        std::vector<std::pair<int32_t, int32_t>> &keyVector);
+    static std::string removeSpaces(const std::string& str);
+    static std::vector<std::string> splitStrings(const std::string& str);
+    static int getBtDeviceKV(int dev_id, std::vector<std::pair<int, int>> &deviceKV,
+        uint32_t codecFormat, bool isAbrEnabled, bool isHostless);
+    static int getDeviceKV(int dev_id, std::vector<std::pair<int, int>> &deviceKV);
+    static bool isBtDevice(int32_t beDevId);
+    static bool compareNumSelectors(struct kvInfo info_1, struct kvInfo info_2);
     PayloadBuilder();
     ~PayloadBuilder();
 };