1f8e7c1e131b0703e65cbc3b7ed71f6c6199d859
Pratyush Meduri <quic_mpratyus@quicinc.com>
2022-02-18 11:14:55 +0530

    configs: neo: Add initial mixer xml files for neo
    Add initial files for neo idp.
    
    Change-Id: Ib83ff78cbfc5c58da5d88314ca896ee3fb207b3e

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1171,6 +1171,7 @@ int ResourceManager::init_audio()
                     strstr(snd_card_name, "sm8150") ||
                     strstr(snd_card_name, "lahaina") ||
                     strstr(snd_card_name, "waipio") ||
+                    strstr(snd_card_name, "neo") ||
                     strstr(snd_card_name, "diwali") ||
                     strstr(snd_card_name, "bengal") ||
                     strstr(snd_card_name, "monaco")) {
