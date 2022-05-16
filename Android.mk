9ba1a6e5262ea41c9d62153dcbf727d1d50262a8
Guodong Hu <quic_guodhu@quicinc.com>
2022-05-16 11:55:02 +0800

    pal: select libqti-tinyalsa for Android 12
    AR-PAL should call into libqti-tinyalsa.
    
    Change-Id: I19e0fb64bffdfdb6fa609e6ddeec8e973c5afd67
    Signed-off-by: Guodong Hu <quic_guodhu@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -123,7 +123,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 #if android version is R, use qtitinyxxx headers & libs, otherwise use upstream ones
 #This assumes we would be using AR code only for Android R and subsequent versions.
-ifneq ($(filter 11 R, $(PLATFORM_VERSION)),)
+ifneq ($(filter 11 R 12, $(PLATFORM_VERSION)),)
 LOCAL_C_INCLUDES       += $(TOP)/vendor/qcom/opensource/tinyalsa/include
 LOCAL_C_INCLUDES       += $(TOP)/vendor/qcom/opensource/tinycompress/include
 LOCAL_SHARED_LIBRARIES += libqti-tinyalsa libqti-tinycompress
