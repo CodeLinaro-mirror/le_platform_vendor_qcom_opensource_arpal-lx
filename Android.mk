2dfaccffc2e89a2f1c41ae94a7763eeb767a99fb
Sitong <quic_sitochen@quicinc.com>
2022-05-18 16:53:50 +0800

    pal: Adapt sndcard monitor for automotive Hypervisor AudioReach
    Automotive targets use /proc/asound/cards as soundcard,
    update the sndcard path and state handling in pal
    
    Change-Id:I52e7d45329f539192e66678a94d3281240ce56eb

--- Android.mk
+++ Android.mk
@@ -20,6 +20,10 @@ LOCAL_CFLAGS        += -DPAL_SP_TEMP_PATH=\"/data/vendor/audio/audio.cal\"
 LOCAL_CFLAGS        += -DACD_SM_FILEPATH=\"/vendor/etc/models/acd/\"
 LOCAL_CPPFLAGS      += -fexceptions -frtti
 
+ifeq ($(PRODUCT_NAME), msmnile_gvmgh)
+  LOCAL_CFLAGS += -DPLATFORM_AUTO
+endif
+
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/stream/inc \
     $(LOCAL_PATH)/device/inc \
