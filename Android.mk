059223bbf906483c5131ef5cc9a768d8e5420f23
Guodong Hu <quic_guodhu@quicinc.com>
2022-10-13 15:34:34 +0800

    pal: populate two new devices to support zone concurrency
    If we start Zone 1/2 playback while Zone 0 playback is still running,
    the unique Device Speaker attribute will get overwritten by Zone 1/2,
    which causes unecpected behaviour of the still-running Zone 0 playback.
    
    With these two new Devices (A2BSpeaker and A2B2Speaker),
    Zone 0/1/2 own seperate Device objs and zone concurrency is supported.
    
    Change-Id: Ibed49a5a480f5c18d3abe4ca7752a9292e026a1b
    Signed-off-by: Guodong Hu <quic_guodhu@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -90,6 +90,8 @@ LOCAL_SRC_FILES := \
     device/src/USBAudio.cpp \
     device/src/Device.cpp \
     device/src/Speaker.cpp \
+    device/src/A2BSpeaker.cpp \
+    device/src/A2B2Speaker.cpp \
     device/src/Bluetooth.cpp \
     device/src/SpeakerMic.cpp \
     device/src/HeadsetMic.cpp \
