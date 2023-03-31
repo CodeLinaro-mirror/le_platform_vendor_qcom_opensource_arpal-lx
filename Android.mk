3930bdb79adfb07606464cc70d6fd872f120025a
Anand Mohan <quic_anamoh@quicinc.com>
2023-03-31 10:29:54 +0530

    pal: add dummy device implementation to pal
    This change contains new device files for dummy device as well as
    its integration in the parent device file.
    
    Change-Id: I67e2f86a989c22a7ecead0641bc7310f1c75e993

--- Android.mk
+++ Android.mk
@@ -88,6 +88,7 @@ LOCAL_SRC_FILES := \
     device/src/ExtEC.cpp \
     device/src/HapticsDev.cpp \
     device/src/UltrasoundDevice.cpp \
+    device/src/DummyDev.cpp \
     session/src/Session.cpp \
     session/src/PayloadBuilder.cpp \
     session/src/SessionAlsaPcm.cpp \
