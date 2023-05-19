f2bebdb5c90fbed78661f3495e503bab9269f676
Kisan YADAV(Temp) <quic_kisayada@quicinc.com>
2023-05-19 12:45:01 +0530

    pal : update logic in getSelectorValues
    - when usecasekv xml is parsed and selector value size is zero, It is
      not pushed in the kv vector list. but when we are retrieving selectors
      values, key pairs are made without checking the size of string.
    - To maintain similarity while parsing and retrieving kv's, Updating
      filled_Selector_pairs by checking the length of selector, if it is
      non zero then only make a pair and push in filled selector list.
    
    Change-Id: Ie09e2c21e91c810a5d330d7071d04ef44b6821cd

--- session/src/PayloadBuilder.cpp
+++ session/src/PayloadBuilder.cpp
@@ -2506,8 +2506,10 @@ std::vector<std::pair<selector_type_t, std::string>> PayloadBuilder::getSelector
                     goto free_sattr;
                 }
 
-                filled_selector_pairs.push_back(std::make_pair(selector_type,
-                    s->getStreamSelector()));
+                if (s->getStreamSelector().length() != 0)
+                    filled_selector_pairs.push_back(std::make_pair(selector_type,
+                        s->getStreamSelector()));
+
                 PAL_INFO(LOG_TAG, "VUI module type:%s", s->getStreamSelector().c_str());
                 break;
             case ACD_MODULE_TYPE_SEL:
@@ -2516,8 +2518,10 @@ std::vector<std::pair<selector_type_t, std::string>> PayloadBuilder::getSelector
                     goto free_sattr;
                 }
 
-                filled_selector_pairs.push_back(std::make_pair(selector_type,
-                    s->getStreamSelector()));
+                if (s->getStreamSelector().length() != 0)
+                    filled_selector_pairs.push_back(std::make_pair(selector_type,
+                        s->getStreamSelector()));
+
                 PAL_INFO(LOG_TAG, "ACD module type:%s", s->getStreamSelector().c_str());
                 break;
             case DEVICEPP_TYPE_SEL:
@@ -2526,8 +2530,10 @@ std::vector<std::pair<selector_type_t, std::string>> PayloadBuilder::getSelector
                     goto free_sattr;
                 }
 
-                filled_selector_pairs.push_back(std::make_pair(selector_type,
-                    s->getDevicePPSelector()));
+                if (s->getDevicePPSelector().length() != 0)
+                    filled_selector_pairs.push_back(std::make_pair(selector_type,
+                        s->getDevicePPSelector()));
+
                 PAL_INFO(LOG_TAG, "devicePP_type:%s", s->getDevicePPSelector().c_str());
                 break;
             case STREAM_TYPE_SEL:
