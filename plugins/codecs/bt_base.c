53d11ecaf3f8e67475a87071dfd2a3744c9e1db9
Puneeth Prabhu <quic_pprabh@quicinc.com>
2024-01-18 11:58:48 +0530

    pal: Add methods to pack aac/sbc decoder config payload
    Change-Id: Idcbd46fb76e25f8dae322a8833edaa84cc34e448
    (cherry picked from commit d6d323f0dcde1ea4ca9b4027b931a9c2d77251d5)

--- plugins/codecs/bt_base.c
+++ plugins/codecs/bt_base.c
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  *
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -108,6 +108,29 @@ int bt_base_populate_enc_cmn_param(custom_block_t *blk, uint32_t param_id,
         return -ENOMEM;
     }
     memcpy(blk->payload, payload, size);
+
+    return 0;
+}
+
+int bt_base_populate_dec_media_fmt(custom_block_t *blk,uint32_t fmt_id,
+        void *payload, size_t size)
+{
+    media_format_t *media_format = NULL;
+    void *enc_payload = NULL;
+
+    blk->param_id = PARAM_ID_MEDIA_FORMAT;
+    blk->payload_sz = sizeof(struct media_format_t) + size;
+    blk->payload = (uint8_t *)calloc(1, blk->payload_sz);
+    if (!blk->payload) {
+        blk->payload_sz = 0;
+        return -ENOMEM;
+    }
+    media_format = (struct media_format_t *) blk->payload;
+    media_format->data_format  = DATA_FORMAT_RAW_COMPRESSED;
+    media_format->fmt_id       = fmt_id;
+    media_format->payload_size = size;
+    enc_payload = (void *)(blk->payload + sizeof(struct media_format_t));
+    memcpy(enc_payload, payload, size);
 
     return 0;
 }
