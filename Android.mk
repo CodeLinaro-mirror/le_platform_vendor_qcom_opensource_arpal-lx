ae831ed054b6772e6fbf04810ffeafad4494f630
Subhadra Jagadeesan <quic_subhjaga@quicinc.com>
2024-05-16 14:12:43 +0530

    pal: Fix build issues with new HAL migration
    Added following changes in header file
    1) Pal OPUS format and decoder struct(ChangeId:I2b498f6248cd84e9ea66cdb2ca32d8787061d55f)
    2) Linear gain for MSPP,
    3) Spatial audio playback stream type (ChangeId:I559be258223b3753ae4d3310bee9bd86c2f1c1f6)
    4) Support for new BLE in and out devices (ChangeId:Ie1ec27faa2ec0b73ea0f3b81b2893a7fc14f9ab5)
    5) Add support for SoundTrigger state parameters(ChangeId:I16ad59af2bae3b82c4684d7b0d6d905d7c38152e)
    
    Change-Id: Ic5e467e93ecb1e998908dc6dec3a0ffe34ab8d80

--- Android.mk
+++ Android.mk
@@ -108,7 +108,8 @@ LOCAL_SRC_FILES := \
     utils/src/ACDPlatformInfo.cpp \
     utils/src/PalRingBuffer.cpp \
     utils/src/SoundTriggerUtils.cpp \
-    utils/src/SignalHandler.cpp
+    utils/src/SignalHandler.cpp \
+    utils/src/MetadataParser.cpp
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EC_REF_CAPTURE)),true)
 LOCAL_SRC_FILES += device/src/ECRefDevice.cpp
 endif
