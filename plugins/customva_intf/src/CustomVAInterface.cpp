d09f5a8c75f07ee57b9c7c63def3a19c389a5db9
SHIKHAR MATHUR <quic_shikmath@quicinc.com>
2024-05-24 06:31:41 -0700

    pal: plugins : Initial porting for changes from tip
    Initial porting for changes from tip
    
    Change-Id: I0208bba5ec148bed1fe43c35919f98f253fc7e2c

--- plugins/customva_intf/src/CustomVAInterface.cpp
+++ plugins/customva_intf/src/CustomVAInterface.cpp
@@ -34,14 +34,11 @@
 #define LOG_TAG "PAL: CustomVAInterface"
 //#define LOG_NDEBUG 0
 
-#ifndef PAL_USE_SYSLOG
 #include <log/log.h>
-#endif
-#include "PalCommon.h"
 #include "CustomVAInterface.h"
-#ifndef PAL_CUTILS_UNSUPPORTED
+
 #include <cutils/properties.h>
-#endif
+
 #define ST_MAX_FSTAGE_CONF_LEVEL  (100)
 #define CUSTOM_CONFIG_OPAQUE_DATA_SIZE 12
 #define CONF_LEVELS_INTF_VERSION_0002 0x02
@@ -112,7 +109,7 @@ CustomVAInterface::CustomVAInterface(
      */
     char value[256] = {0};
 
-#ifndef PAL_CUTILS_UNSUPPORTED
+#ifndef FEATURE_IPQ_OPENWRT
     property_get("vendor.audio.use_qc_wakeup_config", value, "");
     if (!strcmp("true", value))
         use_qc_wakeup_config_ = true;
