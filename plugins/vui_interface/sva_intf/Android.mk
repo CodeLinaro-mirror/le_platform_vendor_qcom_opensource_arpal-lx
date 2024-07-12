4c214f6ff782a8a2cff3c52ef0aaa0eadec0741e
Pratyush Meduri <quic_mpratyus@quicinc.com>
2024-07-12 14:00:14 +0530

    pal: change to Make vui-interface independent of pal
    Move capi_v2 related headers in vui-interface-header and lsm related symbols in VoiceUIInterfaceUtils
    Remove VUIInterfaceProxy header as getVUIInterface related symbols are move to pal
    Add get_vui_interface in svaInterface for making it align with customva and hotword path
    Remove svaExtension related files as set/getparam related SVA enablement is deprecated.
    
    Change-Id: Ic1c63f905ab817939483ce6a53cbe141d950c66a

--- plugins/vui_interface/sva_intf/Android.mk
+++ plugins/vui_interface/sva_intf/Android.mk
@@ -1,14 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libvui_intf
+LOCAL_MODULE := libsva_intf
 LOCAL_MODULE_OWNER := qti
 LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := \
    src/VoiceUIInterface.cpp \
    src/SVAInterface.cpp \
    src/SoundTriggerUtils.cpp \
-   src/SVAExtension.cpp
 
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/inc
