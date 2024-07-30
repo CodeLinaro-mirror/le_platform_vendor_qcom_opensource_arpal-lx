a2879d7faf93cc5d87ead3604d6c1b57f7e87f31
Hanuma Sri Saketh Valluri <quic_sakevall@quicinc.com>
2024-07-30 17:18:41 +0530

    pal: do entire stream close operation with mGraphMutex acquired
    During device start, front end based pcm controls are used to fetch
    miid for tagged modules in device subgraph. Logic for this relies on
    fetching list of active streams on the device and get the pcmId of
    the first active stream.
    
    When one stream is getting closed and another stream is getting started,
    there might come a scenario where session close has finished but device
    close is yet to be done. Another stream which is getting started, will
    get the first stream in the activeStreams list and will try to use its
    pcmId to get the tagged module info. This will lead to crash as pcmId
    is freed as part of the session close.
    
    Fixed this by fully doing stream close with mGraphMutex held. This will
    ensure that state of the stream getting closed has transitioned to IDLE
    when another stream is getting started and it will not be part of active
    streams.
    
    Change-Id: I2fd8edee8970df5664e7b4e5678d2511f6d75e24

--- stream/src/StreamCompress.cpp
+++ stream/src/StreamCompress.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -221,7 +221,6 @@ int32_t StreamCompress::close()
     }
     rm->lockGraph();
     status = session->close(this);
-    rm->unlockGraph();
     if (0 != status) {
         PAL_ERR(LOG_TAG,"session close failed with status %d", status);
     }
@@ -237,6 +236,7 @@ int32_t StreamCompress::close()
     }
     PAL_VERBOSE(LOG_TAG,"closed the devices successfully");
     currentState = STREAM_IDLE;
+    rm->unlockGraph();
     mStreamMutex.unlock();
 
     PAL_DBG(LOG_TAG,"Exit status: %d",status);
