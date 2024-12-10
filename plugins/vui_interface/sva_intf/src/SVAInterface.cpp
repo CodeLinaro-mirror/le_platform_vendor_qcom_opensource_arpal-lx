6bdad32e5fec3706c0bbcf2cc3ca9f3a62a31e7f
Huang, Yidong <quic_yidongh@quicinc.com>
2024-12-10 18:28:31 +0800

    pal: add support for updating ftrt data of MMA usecase
    When extended detection property enabled to pack
    history data in MMA detection payload, use history
    data in ring buffer directly as no keyword index
    is specified in MMA usecase.
    
    Change-Id: Iab154885c3c6a1d5d0b25803d10b67641f3abd3c

--- plugins/vui_interface/sva_intf/src/SVAInterface.cpp
+++ plugins/vui_interface/sva_intf/src/SVAInterface.cpp
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
