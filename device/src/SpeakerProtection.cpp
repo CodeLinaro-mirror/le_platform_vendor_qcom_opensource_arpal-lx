f56402e87e4f568a78b20206e67e1b90c187316f
Sairamreddy Bojja <quic_sbojja@quicinc.com>
2025-01-09 14:12:21 +0000

    pal: fix compilation issue for debian package
    Exclude the glib.h include from the CFLAGS
    
    Change-Id: I0b973beeedb203f524813e50aecaae3bcd6409c6

--- device/src/SpeakerProtection.cpp
+++ device/src/SpeakerProtection.cpp
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, 2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -73,6 +73,7 @@
 
 #include<fstream>
 #include<sstream>
+#include <climits>
 
 #ifndef PAL_SP_TEMP_PATH
 #define PAL_SP_TEMP_PATH "/data/misc/audio/audio.cal"
