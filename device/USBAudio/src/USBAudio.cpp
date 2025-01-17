b4cbf47538779c162fdf002cdd228f6f2fe5af1d
Ramireddy KrishnaKanth Reddy <ramikris@qti.qualcomm.com>
2025-01-17 14:59:30 +0800

    Pal: USB: add direction to get device config for USB
    Add direction to get device config for USB.
    
    Change-Id: I2bca91306d2b7bfc82aeb34ec1379572070b15f7
    (cherry picked from commit 4bb88fe743f08c85d245bdf5c21f2140b522a7b0)

--- device/USBAudio/src/USBAudio.cpp
+++ device/USBAudio/src/USBAudio.cpp
@@ -26,39 +26,10 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "PAL: USB"
@@ -1246,12 +1217,13 @@ int32_t USB::getDeviceConfig(struct pal_device *deviceattr,
     int32_t status = 0;
     struct pal_device_info devinfo = {};
 
-    if (!sAttr) {
+    if (!sAttr || !deviceattr) {
         PAL_ERR(LOG_TAG, "Invalid parameter.");
         return -EINVAL;
     }
+    bool isPlayback = deviceattr->id > PAL_DEVICE_OUT_MAX ? false : true;
     // config.ch_info memory is allocated in selectBestConfig below
-    status = this->selectBestConfig(deviceattr, sAttr, true, &devinfo);
+    status = this->selectBestConfig(deviceattr, sAttr, isPlayback, &devinfo);
     deviceattr->config.aud_fmt_id = bitWidthToFormat.at(deviceattr->config.bit_width);
     if (deviceattr->config.bit_width == BITWIDTH_24) {
         if (devinfo.bitFormatSupported == PAL_AUDIO_FMT_PCM_S24_LE)
