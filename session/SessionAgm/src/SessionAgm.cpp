47afcf2a73354e88eaa544e7e1ba23cd1d2cc7b0
gmanam <quic_gmanam@quicinc.com>
2025-01-19 18:52:06 +0530

    pal: fix some issues for audio-codec2
    1. pal: fix set custom param setting in sessionAGM
      (cherry-picked from 5762173)
    
      payload is sent as void pointer from clients and
      not as param_payload structure so use accordingly.
    
    2. pal: Fix cts issues for c2 codecs
      (cherry-picked from 6002390)
    
      C2 simpleDecode tests are failing due to stream
      open failure which is caused as stream instance
      ids are not reset during previous stream close.
    
      Fix is to reset stream instance ids during non-tunnel
      stream close.
    
    3. pal: update PalApi for swbinder
       add below api:
          pal_stream_set_custom_param
          pal_stream_get_custom_param
          pal_set_custom_param
          pal_get_custom_param
    
       remove below api:
          pal_stream_get_tags_with_module_info
          pal_gef_rw_param
          pal_gef_rw_param_acdb
    
    Change-Id: I912fccf68677a25d274eef8df4d4100456c5e933

--- session/SessionAgm/src/SessionAgm.cpp
+++ session/SessionAgm/src/SessionAgm.cpp
@@ -26,9 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -680,8 +680,7 @@ int32_t SessionAgm::setCustomParam(custom_payload_uc_info_t* uc_info, std::strin
     int status = -EINVAL;
 
     if(param_str == PAL_CUSTOM_PARAM_AR_TAG_MODULE_CONFIG) {
-        pal_param_payload *payload = (pal_param_payload *)param_payload;
-        status = agm_session_set_params(sessionId, payload->payload, payload->payload_size);
+        status = agm_session_set_params(sessionId, param_payload, payload_size);
         if (0 != status)
             PAL_ERR(LOG_TAG,"setTagsWithModuleInfo Failed");
     }
