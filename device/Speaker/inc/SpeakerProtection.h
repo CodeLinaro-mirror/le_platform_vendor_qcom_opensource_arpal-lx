c6ab3c7c3f47d6bb8b5d6c3d2de615c4efb35f53
Faiz Nabi Kuchay <quic_fkuchay@quicinc.com>
2025-01-20 17:59:07 +0530

    PAL: SP: process vi-tx and general rx in parallel threads
    process vi-tx and general rx in parallel threads.
    
    Change-Id: I85fb4755a822713c2223889ba1cbb1e537b7a0e2

--- device/Speaker/inc/SpeakerProtection.h
+++ device/Speaker/inc/SpeakerProtection.h
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, 2025, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -140,6 +140,7 @@ protected :
     static bool isSpkrInUse;
     static bool calThrdCreated;
     static bool isDynamicCalTriggered;
+    static bool viTxSetupThrdCreated;
     static struct timespec spkrLastTimeUsed;
     static struct mixer *virtMixer;
     static struct mixer *hwMixer;
@@ -156,11 +157,14 @@ protected :
     static int numberOfRequest;
     static struct pal_device_info vi_device;
     static struct pal_device_info cps_device;
+    void *viCustomPayload;
+    size_t viCustomPayloadSize;
 
 private :
 
 public:
     static std::thread mCalThread;
+    static std::thread viTxSetupThread;
     static std::condition_variable cv;
     static std::mutex cvMutex;
     std::mutex deviceMutex;
@@ -170,6 +174,7 @@ public:
     int32_t getSpeakerTemperature();
     void spkrCalibrateWait();
     int spkrStartCalibration();
+    int viTxSetupThreadLoop();
     void speakerProtectionInit();
     void speakerProtectionDeinit();
     void getSpeakerTemperatureList();
@@ -194,6 +199,7 @@ public:
     static void handleSPCallback (uint64_t hdl, uint32_t event_id, void *event_data,
                                   uint32_t event_size);
     void updateCpsCustomPayload(int miid);
+    int updateVICustomPayload(void *payload, size_t size);
     int getCpsDevNumber(std::string mixer);
     int32_t getCalibrationData(void **param);
     int32_t getFTMParameter(void **param);
