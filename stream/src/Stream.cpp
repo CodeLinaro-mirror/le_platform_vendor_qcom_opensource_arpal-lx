9e329b0954a1fdedc05cec54f0b320c3fe2aaa53
qianch <quic_qianch@quicinc.com>
2025-02-25 17:54:19 +0800

    PAL: Check and mute/unmute stream during force switching
    If the stream was implicated due to sharing same backend during dev
    switching, It will be forcibly switched which might bring in pop
    noise due to underrun.
    Check and mute it before switching and unmute after switching done
    to avoid unexpected pop noise issue.
    
    Change-Id: Iefc929d67aaf90310d72bce5373f1e3b93e0661a
    (cherry picked from commit 888eb155a18f55e8161161601101eba1e5c5597f)

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -26,9 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -1642,6 +1642,8 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
     struct pal_volume_data *volume = NULL;
     pal_device_id_t newBtDevId;
     bool isBtReady = false;
+    std::vector <Stream *> tempMutedStreams;
+
     rm->lockActiveStream();
     mStreamMutex.lock();
 
@@ -1838,8 +1840,23 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
                 }
                 /* If prioirty based attr diffs with running dev switch all devices */
                 if (switchStreams || custom_switch) {
+                    pal_stream_attributes sAttr;
                     streamDevDisconnect.push_back(elem);
                     StreamDevConnect.push_back({std::get<0>(elem), &newDevices[newDeviceSlots[i]]});
+                    if (sharedStream != streamHandle) {
+                        /*
+                         * temporarily mute streams for implicit device switch due to BE shared
+                         * with streamHandle.
+                         */
+                        sharedStream->getStreamAttributes(&sAttr);
+                        if (sAttr.type == PAL_STREAM_DEEP_BUFFER ||
+                            sAttr.type == PAL_STREAM_COMPRESSED ||
+                            sAttr.type == PAL_STREAM_PCM_OFFLOAD) {
+                            PAL_DBG(LOG_TAG, "mute stream %pk during switching", sharedStream);
+                            sharedStream->mute(true);
+                            tempMutedStreams.push_back(sharedStream);
+                        }
+                    }
                     matchFound = true;
                     custom_switch = false;
                 } else {
@@ -2015,6 +2032,13 @@ int32_t Stream::switchDevice(Stream* streamHandle, uint32_t numDev, struct pal_d
     }
 
 done:
+    if (!tempMutedStreams.empty()) {
+        for(sIter = tempMutedStreams.begin(); sIter != tempMutedStreams.end(); sIter++) {
+            (*sIter)->mute(false);
+            PAL_DBG(LOG_TAG, "unmute stream %pk during switching", *sIter);
+        }
+    }
+    tempMutedStreams.clear();
     mStreamMutex.lock();
     if (a2dpMuted) {
         if (mVolumeData) {
