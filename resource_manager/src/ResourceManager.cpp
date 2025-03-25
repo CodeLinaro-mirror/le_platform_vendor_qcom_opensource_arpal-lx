4100469c918f2eb1bd8b4bf47695d933d42cd92e
Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>
2025-03-25 16:33:56 +0530

    PAL: Fix and invalid pointer de-reference while deRegisterdevice
    When one input and output stream are going to standby respecitvely
    the input stream's stream object is being accessed by deRegister of output
    stream causing a crash added a mutex lock to avoid crash.
    
    Suggested-by: Venu Gopal Atchyutanna  <vatchyut@qti.qualcomm.com>
    Change-Id: I3e950cf907daf5732b9e4c217a5464e1a93e7b7e
    Signed-off-by: Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -4652,7 +4652,7 @@ std::vector<Stream*> ResourceManager::getConcurrentTxStream_l(
         status = -EINVAL;
         goto exit;
     }
-
+    mActiveStreamMutex.lock();
     for (auto& tx_str: mActiveStreams) {
         tx_device_list.clear();
         tx_str->getStreamAttributes(&tx_attr);
@@ -4680,6 +4680,7 @@ std::vector<Stream*> ResourceManager::getConcurrentTxStream_l(
             }
         }
     }
+    mActiveStreamMutex.unlock();
 exit:
     return tx_stream_list;
 }
