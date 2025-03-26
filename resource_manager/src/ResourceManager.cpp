d24cee88c105d3696022324a765c50371c43385a
Qingyue Chen <quic_qingyuec@quicinc.com>
2025-03-27 01:04:40 +0800

    pal: ASRC support
    Change-Id: I7c3ed99e53b55b93224bf31fd56a84196c2f43d1
    (cherry picked from commit be277e37bdfce31a987944d17ec27b35f32d568b)

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -8610,6 +8610,33 @@ int ResourceManager::setParameter(uint32_t param_id, void *param_payload,
             }
         }
         break;
+        case PAL_PARAM_ID_ASRC:
+        {
+            std::list<Stream*>::iterator sIter;
+            pal_stream_attributes sAttr;
+
+            for(sIter = mActiveStreams.begin(); sIter != mActiveStreams.end(); sIter++) {
+
+                status = (*sIter)->getStreamAttributes(&sAttr);
+                if (0 != status) {
+                    PAL_ERR(LOG_TAG, "Failed to get attributes, status= %d", status);
+                    return status;
+                }
+
+                if(strcmp(sAttr.bus_addr, "BUS00_MEDIA") == 0){
+                    status = (*sIter)->setParameters(PAL_PARAM_ID_ASRC, param_payload);
+                    if (status) {
+                        PAL_ERR(LOG_TAG, "Failed to set ASRC");
+                        goto exit;
+                    }
+                    goto exit;
+                }
+            }
+            PAL_ERR(LOG_TAG, "Failed to find the BUS00_MEDIA stream");
+            status = -EIO;
+            goto exit;
+        }
+        break;
         default:
             PAL_ERR(LOG_TAG, "Unknown ParamID:%d", param_id);
             break;
