c159de2d1bfb280c88915f5fc4318e133ca3ec5c
Balakrishna Kunthumuri <quic_kunthumu@quicinc.com>
2025-05-05 15:36:35 +0530

    PAL: [RBVM] Fix klocwork violations
    Change-Id: I080debb8a173867c7f74334e3dcf60d6ebbecd77

--- plugins/controls/oemPluginControls.cpp
+++ plugins/controls/oemPluginControls.cpp
@@ -29,7 +29,7 @@
 
 /*
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2022,2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -184,7 +184,7 @@ int payloadCalKeys(Session* s, uint8_t **payload, size_t *size, float volume)
     apm_module_param_data_t* header;
     uint8_t* payloadInfo = NULL;
     size_t payloadSize = 0, padBytes = 0;
-    uint8_t *vol_pl;
+    uint8_t *vol_pl = NULL;
     vcpm_param_cal_keys_payload_t cal_keys;
     vcpm_ckv_pair_t cal_key_pair[NUM_OF_CAL_KEYS];
     int vol;
