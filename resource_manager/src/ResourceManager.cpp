fb33fd056b46c5b6efa51e95176a37cdb3b6fe07
Afroza Nasrin <sufiafro@qti.qualcomm.com>
2025-05-14 22:41:10 +0530

    pal: Add support for alor in init_audio.
    Add support for alor during init_audio to enable finding of
    alor sound card
    
    Change-Id: If440ebfe02f4e13e163c08c482372fe38f24c4a1

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1499,6 +1499,7 @@ int ResourceManager::init_audio()
                     strstr(snd_card_name, "bengal") ||
                     strstr(snd_card_name, "monaco") ||
                     strstr(snd_card_name, "canoe") ||
+                    strstr(snd_card_name, "alor") ||
                     strstr(snd_card_name, "sun")) {
                     PAL_VERBOSE(LOG_TAG, "Found Codec sound card");
                     snd_card_found = true;
