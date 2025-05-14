5f68b73522e8ab4b1a6f8600d56a99ee43ab0947
Anjaly Arjunan <aarjunan@qti.qualcomm.com>
2025-05-14 17:30:46 +0530

    PAL : Adding 17 channel support in audiohal.
    Adding the enum for 17 channel support in the required files
    
    Change-Id: I4c039e4507e034ff1a47b2e9462738e60979dd57

--- session/src/PayloadBuilder.cpp
+++ session/src/PayloadBuilder.cpp
@@ -445,6 +445,25 @@ void PayloadBuilder::populateChannelMap(T pcmChannel, uint8_t numChannel)
         pcmChannel[14] = PCM_CHANNEL_RLC;
         pcmChannel[15] = PCM_CHANNEL_RRC;
     }
+    else if (numChannel == 17) {
+        pcmChannel[0] = PCM_CHANNEL_L;
+        pcmChannel[1] = PCM_CHANNEL_R;
+        pcmChannel[2] = PCM_CHANNEL_C;
+        pcmChannel[3] = PCM_CHANNEL_LS;
+        pcmChannel[4] = PCM_CHANNEL_RS;
+        pcmChannel[5] = PCM_CHANNEL_LFE;
+        pcmChannel[6] = PCM_CHANNEL_CS;
+        pcmChannel[7] = PCM_CHANNEL_LB;
+        pcmChannel[8] = PCM_CHANNEL_RB;
+        pcmChannel[9] = PCM_CHANNEL_TS;
+        pcmChannel[10] = PCM_CHANNEL_CVH;
+        pcmChannel[11] = PCM_CHANNEL_MS;
+        pcmChannel[12] = PCM_CHANNEL_FLC;
+        pcmChannel[13] = PCM_CHANNEL_FRC;
+        pcmChannel[14] = PCM_CHANNEL_RLC;
+        pcmChannel[15] = PCM_CHANNEL_RRC;
+        pcmChannel[16] = PCM_CHANNEL_LFE2;
+    }
 }
 
 void PayloadBuilder::payloadUsbAudioConfig(uint8_t** payload, size_t* size,
