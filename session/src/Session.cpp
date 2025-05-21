6623ef9399c52d5dba6897fdf7cb95d8a47f092b
GOWTHAM BHARANI MUDILI <gmudili@qti.qualcomm.com>
2025-05-21 09:44:32 -0700

    Revert "pal: Fix compilation errors of palserver and palclient"
    This reverts commit 60cdaa3ab81f258970f929912278640da3455167.
    
    Reason for revert: Compilation failure on top of 201AU
    
    Change-Id: I29b212c1dc9cf21be3d74d9fcbf64a5a3999e9f1

--- session/src/Session.cpp
+++ session/src/Session.cpp
@@ -26,9 +26,39 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Technologies, Inc. are provided under the following license:
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ *
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define LOG_TAG "PAL: Session"
@@ -36,11 +66,6 @@
 #include "Session.h"
 #include "ResourceManager.h"
 #include "PalMappings.h"
-#include "SessionAlsaPcm.h"
-#include "SessionAlsaCompress.h"
-#include "SessionAgm.h"
-#include "SessionAlsaUtils.h"
-#include "SessionAlsaVoice.h"
 
 std::shared_ptr<PluginManager> Session::pm = nullptr;
 
@@ -87,44 +112,3 @@ Session* Session::makeSession(const std::shared_ptr<ResourceManager>& rm, const 
     }
     return s;
 }
-
-Session* Session::makeACDBSession(const std::shared_ptr<ResourceManager>& rm,
-                                    const struct pal_stream_attributes *sAttr)
-{
-    if (!rm || !sAttr) {
-        PAL_ERR(LOG_TAG,"Invalid parameters passed");
-        return nullptr;
-    }
-
-    Session* s = (Session*) nullptr;
-    s = new SessionAgm(rm);
-
-    return s;
-}
-
-int Session::rwACDBParamTunnel(void *payload, pal_device_id_t palDeviceId,
-                        pal_stream_type_t palStreamType, uint32_t sampleRate,
-                        uint32_t instanceId, bool isParamWrite, Stream * s)
-{
-    int status = -EINVAL;
-    struct pal_stream_attributes sAttr = {};
-
-    PAL_DBG(LOG_TAG, "Enter");
-    status = s->getStreamAttributes(&sAttr);
-    streamHandle = s;
-    if (0 != status) {
-        PAL_ERR(LOG_TAG,"getStreamAttributes Failed \n");
-        goto exit;
-    }
-
-    PAL_INFO(LOG_TAG, "PAL device id=0x%x", palDeviceId);
-    status = SessionAlsaUtils::rwACDBTunnel(s, rm, palDeviceId, payload, isParamWrite, instanceId);
-    if (status) {
-        PAL_ERR(LOG_TAG, "session alsa open failed with %d", status);
-    }
-
-exit:
-    PAL_DBG(LOG_TAG, "Exit status: %d", status);
-    return status;
-}
-
