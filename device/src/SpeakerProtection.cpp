85e6f1f0a09eca60ff4d057d65f47c416fc99228
Vaibhav Raut <quic_vraut@quicinc.com>
2025-05-29 18:30:15 +0530

    Fix accessing index out of array boundary.
    pal: Adjusted loop limit to match actual r0t0Array
    size to ensure safe access within valid index
    limits.
    
    Change-Id: Ic74fce600991b3f95514112e686a7cd48ea08c4f

--- device/src/SpeakerProtection.cpp
+++ device/src/SpeakerProtection.cpp
@@ -26,39 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "PAL: SpeakerProtection"
@@ -1870,7 +1840,7 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
         PAL_DBG(LOG_TAG, "Read R0T0 from file");
         fp = fopen(PAL_SP_TEMP_PATH, "rb");
         if (fp) {
-            for (int i = 0; i < vi_device.channels; i++) {
+            for (int i = 0; i < numberOfChannels; i++) {
                 fread(&r0t0Array[i].r0_cali_q24,
                       sizeof(r0t0Array[i].r0_cali_q24), 1, fp);
                 fread(&r0t0Array[i].t0_cali_q6,
@@ -1880,7 +1850,7 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
         }
         else {
             PAL_DBG(LOG_TAG, "Speaker not calibrated. Send safe value");
-            for (int i = 0; i < vi_device.channels; i++) {
+            for (int i = 0; i < numberOfChannels; i++) {
                 r0t0Array[i].r0_cali_q24 = MIN_RESISTANCE_SPKR_Q24;
                 r0t0Array[i].t0_cali_q6 = SAFE_SPKR_TEMP_Q6;
             }
