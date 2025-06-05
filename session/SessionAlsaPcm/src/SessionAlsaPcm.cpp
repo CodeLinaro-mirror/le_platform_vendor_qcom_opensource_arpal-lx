545f3d6f19a4da11d28d118d35c58f3c7900b51b
Venu Gopal Atchyutanna <vatchyut@qti.qualcomm.com>
2025-06-05 10:33:04 +0530

    PAL: Fix the payload size check failure for shared memory tests
    For shared memory testcases,
    payload size can vary till 32MB, so skipping the payload sizecheck
    
    Change-Id: I1daa1f78cd8c08da107c5f64fa3fb646d011d488
    Signed-off-by: Venu Gopal Atchyutanna <vatchyut@qti.qualcomm.com>

--- session/SessionAlsaPcm/src/SessionAlsaPcm.cpp
+++ session/SessionAlsaPcm/src/SessionAlsaPcm.cpp
@@ -4073,7 +4073,13 @@ int SessionAlsaPcm::getParamWithTag(Stream *s __unused, int tagId, uint32_t para
         goto exit;
     }
 
-    if (payloadData && payloadSize <= MAX_PCM_PAYLOAD_SIZE) {
+    if (param_id == PAL_PARAM_ID_PLUGIN_PARAM ) {
+        status = mixer_ctl_get_array(ctl, payloadData, payloadSize);
+        if (0 != status) {
+            PAL_ERR(LOG_TAG, "Get custom config failed, status = %d", status);
+            goto exit;
+        }
+    } else if (payloadData && payloadSize <= MAX_PCM_PAYLOAD_SIZE ) {
         status = mixer_ctl_get_array(ctl, payloadData, payloadSize);
         if (0 != status) {
             PAL_ERR(LOG_TAG, "Get custom config failed, status = %d", status);
@@ -4085,7 +4091,7 @@ int SessionAlsaPcm::getParamWithTag(Stream *s __unused, int tagId, uint32_t para
             status = -ENOMEM;
             goto exit;
         } else {
-            PAL_ERR(LOG_TAG, "Payloadsize exceeds max permissible value");
+            PAL_ERR(LOG_TAG, "Payload size %d exceeds max permissible value %d", payloadSize, MAX_PCM_PAYLOAD_SIZE);
             status = -EINVAL;
             goto exit;
         }
