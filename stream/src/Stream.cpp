d22652936f64ab3c767703f8ba7211a2ae8aee58
Qiang <qianch@qti.qualcomm.com>
2025-06-11 15:50:11 +0800

    PAL: protect session with graph mutex
    The device CKV may be modified by other threads in session due to
    race condition issue.
    Protect setupSessionDevice with graph mutex.
    Add device mutex in getDeviceCount() to avoid race condition issue.
    
    Change-Id: I4fad62201fa3a770dc62a9285a3bec7cbe0229a7

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -1202,7 +1202,9 @@ int32_t Stream::handleBTDeviceNotReady(bool& a2dpSuspend)
             }
 
             mDevices.push_back(dev);
+            rm->lockGraph();
             status = session->setupSessionDevice(this, mStreamAttr->type, dev);
+            rm->unlockGraph();
             if (0 != status) {
                 PAL_ERR(LOG_TAG, "setupSessionDevice failed:%d", status);
                 dev->close();
@@ -1371,14 +1373,15 @@ int32_t Stream::connectStreamDevice_l(Stream* streamHandle, struct pal_device *d
     }
 
     mDevices.push_back(dev);
+    rm->lockGraph();
     status = session->setupSessionDevice(streamHandle, mStreamAttr->type, dev);
     if (0 != status) {
         PAL_ERR(LOG_TAG, "setupSessionDevice for %d failed with status %d",
                 dev->getSndDeviceId(), status);
+        rm->unlockGraph();
         goto dev_close;
     }
 
-    rm->lockGraph();
     if (currentState != STREAM_INIT && currentState != STREAM_STOPPED) {
         status = dev->start();
         if (0 != status) {
