7f58a1203768026d803b3e961837272c6cba0739
Prasanna Kumar Reddy Mukka <pmukka@qti.qualcomm.com>
2025-06-23 13:05:32 +0530

    Revert: PAL: Fix and invalid pointer de-reference while deRegisterdevice
    During SSR, the the framework pause command is blocked during pal_stream_close
    
    Change-Id: I132530268600e834d90c3dfb792208be8fec261e
    Signed-off-by: Prasanna Kumar Reddy Mukka <pmukka@qti.qualcomm.com>

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -4685,7 +4685,6 @@ std::vector<Stream*> ResourceManager::getConcurrentTxStream_l(
         status = -EINVAL;
         goto exit;
     }
-    mActiveStreamMutex.lock();
     for (auto& tx_str: mActiveStreams) {
         tx_device_list.clear();
         tx_str->getStreamAttributes(&tx_attr);
@@ -4713,7 +4712,6 @@ std::vector<Stream*> ResourceManager::getConcurrentTxStream_l(
             }
         }
     }
-    mActiveStreamMutex.unlock();
 exit:
     return tx_stream_list;
 }
