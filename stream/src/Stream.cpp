204f58b3573c566dda3660049b69299fa8396e4f
gmanam <quic_gmanam@quicinc.com>
2025-07-15 19:13:22 +0530

    pal: add new mutex to synchronize getParam mixer ctl call to AGM
    Earlier getParam call to AGM was synchronized with resourceManager
    mutex. This was causing delay for get parameters in Resource Manager,
    as it will be waiting for resourceManager mutex lock.
    
    In cases where there was a concurrency of low latency and offloaded
    stream on BT, get encoder latency param for ll stream was getting
    blocked due to resourceManager mutex previously acquired by getTimestamp
    call for offloaded stream. This was leading to delay in write calls for
    ll stream, and thus causing glitches over BT.
    
    Fixed this by introducing mGetParamMutex, which will be used to sync
    getParam mixer control calls to AGM.
    
    Change-Id: I1486e7e2920e28d252c3139b03be6291a8dff840

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -852,9 +852,9 @@ int32_t Stream::getTimestamp(struct pal_session_time *stime)
         PAL_ERR(LOG_TAG, "Sound card offline/standby, status %d", status);
         goto exit;
     }
-    rm->lockResourceManagerMutex();
+    lockGetParamMutex();
     status = session->getTimestamp(stime);
-    rm->unlockResourceManagerMutex();
+    unlockGetParamMutex();
     if (0 != status) {
         PAL_ERR(LOG_TAG, "Failed to get session timestamp status %d", status);
         if (errno == -ENETRESET &&
@@ -1920,7 +1920,9 @@ int32_t Stream::getCustomParam(custom_payload_uc_info_t* uc_info, std::string pa
         mStreamMutex.unlock();
         return -EINVAL;
     }
+    lockGetParamMutex();
     status = session->getCustomParam(uc_info, param_str, param_payload, payload_size, this);
+    unlockGetParamMutex();
     if (status) {
        PAL_ERR(LOG_TAG, "getCustomParam failed with %d", status);
     }
