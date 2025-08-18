00e77152309d63e70b9d41a68abbdc48af83ff32
Huang, Yidong <yidongh@qti.qualcomm.com>
2025-08-18 16:12:11 +0800

    pal: refactor lpi usage query logic for ST usecases
    ST streams may query from STUtils on whether to use lpi
    or not with GetLPIUsage api, which may give wrong result
    if it is called when there's a deferred LPI-NLPI switch.
    For VoiceUI usecase, it may cause NLPI graphs created
    during device switch if there's a pending LPI-NLPI switch,
    while in gsl engine lpi flag is still true and this
    lpi state mismatch will cause failure in ADSP param set.
    
    Refactor lpi usage query logic to always return use_lpi_
    flag as it is changed only when LPI-NLPI actually happens.
    
    Change-Id: Iee2a0f455ceab487ac8daf6295e708a2641b9f6d

--- stream/StreamASR/src/StreamASR.cpp
+++ stream/StreamASR/src/StreamASR.cpp
@@ -528,7 +528,7 @@ int32_t StreamASR::setECRef(std::shared_ptr<Device> dev, bool isEnable)
     int32_t status = 0;
 
     std::lock_guard<std::mutex> lck(mStreamMutex);
-    if (getLPIUsage()) {
+    if (ConfigSupportLPI() && getLPIUsage()) {
         PAL_INFO(LOG_TAG, "EC ref will be handled in LPI/NLPI switch");
         return status;
     }
@@ -596,7 +596,7 @@ std::shared_ptr<CaptureProfile> StreamASR::GetCurrentCaptureProfile()
     if (!UseLpiCaptureProfile())
         registerNLPIStream(this);
 
-    if (getLPIUsage())
+    if (ConfigSupportLPI() && getLPIUsage())
         operatingMode = ST_OPERATING_MODE_LOW_POWER;
 
     capProf = smCfg->GetCaptureProfile(
@@ -758,7 +758,7 @@ int32_t StreamASR::SetupDetectionEngine()
         }
     }
 
-    if (getLPIUsage() &&
+    if (ConfigSupportLPI() && getLPIUsage() &&
        !UseLpiCaptureProfile()) {
         mStreamMutex.unlock();
         registerNLPIStream(this);
@@ -1204,7 +1204,7 @@ int32_t StreamASR::ASRActive::ProcessEvent(
                 PAL_ERR(LOG_TAG, "Error:%d Device close failed", status);
             asrStream.deviceOpened = false;
 
-            if (getLPIUsage() &&
+            if (asrStream.ConfigSupportLPI() && getLPIUsage() &&
                 !asrStream.UseLpiCaptureProfile()) {
                 asrStream.mStreamMutex.unlock();
                 forceSwitchSoundTriggerStreams(false);
