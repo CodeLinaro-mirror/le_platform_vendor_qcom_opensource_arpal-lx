9ab73d27d7f165955ed63ee4a6e4bef0949ccfb8
Kisan Yadav <kisany@qti.qualcomm.com>
2025-08-26 19:06:32 +0530

    HapticsDev: RM: vote for sleep monitor for calibration mode
    There is no adsp sleep monitor voting for calibration mode.
    There is adsp no sleep issue seen with haptics dynamic calibration
    which gets trigger every 30 minutes.
    We suspect that its a false alarm due to calibration mode running and
    adsp being still active.
    Adding adsp sleep monitor for calibration and processing mode.
    
    Change-Id: Ib1c99d0ed12005d3ffa826778c94c0002e413319

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1885,6 +1885,13 @@ int32_t ResourceManager::voteSleepMonitor(Stream *str, bool vote, bool force_nlp
     }
 
     monitor_payload.version = ADSPSLEEPMON_IOCTL_AUDIO_VER_1;
+
+    //For calibration mode as we dont have stream associated so skipping it
+    if (str == nullptr) {
+        lpi_stream = 0;
+        goto update_sleep_mon;
+    }
+
     ret = str->getStreamType(&type);
     if (ret != 0) {
         PAL_ERR(LOG_TAG, "getStreamType failed with status : %d", ret);
@@ -1902,6 +1909,7 @@ int32_t ResourceManager::voteSleepMonitor(Stream *str, bool vote, bool force_nlp
                       !IsTransitToNonLPIOnChargingSupported());
     }
 
+update_sleep_mon:
     mSleepMonitorMutex.lock();
     if (vote) {
         if (lpi_stream) {
