4bbe674d47a61c66441ff96e522801432feadffc
Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>
2025-09-08 17:45:05 +0530

    pal: vote for sleep monitor for calib/normal mode
    There is no adsp sleep monitor voting for calibration mode,
    incase of no event from dsp, hlos will wait for few seconds
    before closing the graph, if adsp sleep mon triggers at that
    point we may see a crash.
    Add vote for sleep moniotor when speaker protection
    modules were active to ensure the ignore the adsp sleep
    check.
    
    Change-Id: I3816192debd0b9a210576056b5d7f47a2e2594c0

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1888,7 +1888,6 @@ int32_t ResourceManager::voteSleepMonitor(Stream *str, bool vote, bool force_nlp
 
     //For calibration mode as we dont have stream associated so skipping it
     if (str == nullptr) {
-        lpi_stream = 0;
         goto update_sleep_mon;
     }
 
