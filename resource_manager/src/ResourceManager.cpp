773dbf484a9b6936e2c12780008b2e4e281f4d5b
Faiz Nabi Kuchay <fkuchay@qti.qualcomm.com>
2025-09-11 09:46:11 +0530

    PAL: RM: use conditional AND instead of logical
    For conditions where streamDevDisconnect.size is even, the check
    to switch the stream fails due to unintended & instead of &&, which
    might lead to unusual error.
    
    Add conditional && instead of logical & to fix this.
    
    Change-Id: Ife488a6853539604674f3b0ad5cb1c6936a387dd

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -6572,7 +6572,7 @@ bool ResourceManager::updateDeviceConfig(std::shared_ptr<Device> *inDev,
 
     // if device switch is needed, perform it
     // for i2s dual mono, there is no need to switch as the conf is fixed for all use-cases
-    if (streamDevDisconnect.size() & !IsI2sDualMonoEnabled()) {
+    if (streamDevDisconnect.size() && !IsI2sDualMonoEnabled()) {
         status = streamDevSwitch(streamDevDisconnect, streamDevConnect);
         if (status) {
             PAL_ERR(LOG_TAG, "deviceswitch failed with %d", status);
@@ -10131,7 +10131,7 @@ void ResourceManager::restoreDevice(std::shared_ptr<Device> dev)
     }
 
     mActiveStreamMutex.unlock();
-    if (!streamDevDisconnect.empty() & !IsI2sDualMonoEnabled())
+    if (!streamDevDisconnect.empty() && !IsI2sDualMonoEnabled())
         streamDevSwitch(streamDevDisconnect, streamDevConnect);
 exit:
     PAL_DBG(LOG_TAG, "Exit");
