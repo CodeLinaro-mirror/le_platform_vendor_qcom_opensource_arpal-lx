19f487fd5bd307d4d7cb39f4a8b825fd450768a3
Huang, Yidong <yidongh@qti.qualcomm.com>
2025-09-22 15:51:18 +0800

    pal: avoid wakelock release for device switch during detection
    Wakelock release happens when device switch comes in detected
    or buffering state, which will be a problem as device switch
    does not change stream state. When client app tries to restart
    for next detection, wake lock will be released again which is
    unexpected.
    
    Remove wakelock release for device switch in detected and
    buffering state and wait for client restart/stop to trigger
    wakelock release.
    
    Change-Id: I027fd7bfad850779c55bff9af56091cc9a11c251
    (cherry picked from commit e160e549b38c152a244836cf24cd42822e5e2a0a)

--- stream/StreamSoundTrigger/src/StreamSoundTrigger.cpp
+++ stream/StreamSoundTrigger/src/StreamSoundTrigger.cpp
@@ -3307,7 +3307,6 @@ int32_t StreamSoundTrigger::StDetected::ProcessEvent(
         }
         case ST_EV_DEVICE_DISCONNECTED: {
             status = st_stream_.DisconnectEvent(ev_cfg);
-            rm->releaseWakeLock();
             break;
         }
         case ST_EV_DEVICE_CONNECTED: {
@@ -3654,7 +3653,6 @@ int32_t StreamSoundTrigger::StBuffering::ProcessEvent(
             if (st_stream_.reader_) {
                 st_stream_.reader_->reset();
             }
-            rm->releaseWakeLock();
             break;
         }
         case ST_EV_DEVICE_CONNECTED: {
