00a1d66dcf18050409f20370c3aef7754c390149
Eruvaram Kumar Raja Reddy <erajared@qti.qualcomm.com>
2025-10-27 17:19:15 +0530

    pal: update absolute path for kernel based on kernel version
    Change-Id: I843223dd8149a8ee49ab090c32fc8f752d4a7801

--- Android.mk
+++ Android.mk
@@ -44,7 +44,11 @@ ifneq ($(TARGET_KERNEL_VERSION), 4.19)
 ifneq ($(TARGET_KERNEL_VERSION), 4.4)
 ifneq ($(TARGET_KERNEL_VERSION), 4.9)
 ifneq ($(TARGET_KERNEL_VERSION), 5.4)
+ifeq ($(TARGET_KERNEL_VERSION), 6.12)
+LOCAL_C_INCLUDES += $(TOP)/kernel_platform/soc-repo/include/uapi/misc
+else
 LOCAL_C_INCLUDES += $(TOP)/kernel_platform/msm-kernel/include/uapi/misc
+endif
 endif
 endif
 endif
