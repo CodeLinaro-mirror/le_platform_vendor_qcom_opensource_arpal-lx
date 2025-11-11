35ae67cef848255be6f085abc979ac6620e75cbe
Abinath S <abins@qti.qualcomm.com>
2025-11-11 14:55:04 +0530

    pal: Add a new API to check the availability of bt input devices
    Change-Id: I27d74775b292501067e17b2fd0bd4d0a1cd171d3

--- Pal.cpp
+++ Pal.cpp
@@ -26,40 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define LOG_TAG "PAL: API"
@@ -1430,4 +1399,23 @@ int32_t pal_get_mic_mute(bool *state){
 int32_t pal_set_mic_mute(bool state){
     PAL_ERR(LOG_TAG, "error: API: pal_set_mic_mute not implemented");
     return -ENOSYS;
+}
+
+bool pal_bt_input_device_available(pal_device_id_t pal_device_id)
+{
+    std::shared_ptr<ResourceManager> rm = NULL;
+    PAL_VERBOSE(LOG_TAG, "Enter:");
+    rm = ResourceManager::getInstance();
+
+    if (!rm) {
+        PAL_ERR(LOG_TAG, "PAL not initialized, cannot check device availability");
+        return false;
+    }
+
+    if (!rm->isDeviceAvailable(pal_device_id)) {
+        PAL_ERR(LOG_TAG, "bt input device %d is not available", pal_device_id);
+        return false;
+    }
+
+    return true;
 }
