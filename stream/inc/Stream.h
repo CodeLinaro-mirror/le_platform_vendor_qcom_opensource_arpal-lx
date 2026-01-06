36de1747d0bbdabf5a77ea733ef31e6b21db1cf7
Snigdha Upadhyay <snigupad@qti.qualcomm.com>
2026-01-06 10:24:09 +0530

    pal : Extend support to reset mmap buffer.
    Add pal api in StreamPcm class to reset mmap buffer
    so that it can be called from HAL.
    
    Change-Id: I5893fdfbec2b83cfd040032ae73f12c5c372c6f0

--- stream/inc/Stream.h
+++ stream/inc/Stream.h
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -245,6 +245,7 @@ public:
     virtual int32_t flush() {return 0;}
     virtual int32_t suspend() {return 0;}
     virtual int32_t read(struct pal_buffer *buf) = 0;
+    virtual int32_t ResetMmapBuff() {return 0;}
 
     virtual int32_t addRemoveEffect(pal_audio_effect_t effect, bool enable) = 0; //TBD: make this non virtual and prrovide implementation as StreamPCM and StreamCompressed are doing the same things
     virtual int32_t setParameters(uint32_t param_id, void *payload) = 0;
