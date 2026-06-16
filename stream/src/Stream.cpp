67f14be040d67a3bce7995e77bacaea596f3fa59
Shijie Lin <shijlin@qti.qualcomm.com>
2026-06-16 18:19:14 +0800

    PAL: fix speaker count leak on A2DP suspend.
    Use the iterator returned by erase() when removing a stream device entry so Speaker count and routing state stay consistent after playback retries with suspended A2DP.
    
    Change-Id: Ib3be0f541fb3c5189a8feb6f979f30bfec6e004f

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -1883,7 +1883,7 @@ void Stream::removemDevice(int palDevId)
     for (dIter = mDevices.begin(); dIter != mDevices.end();) {
         devId = (*dIter)->getSndDeviceId();
         if (devId == palDevId) {
-            mDevices.erase(dIter);
+            dIter = mDevices.erase(dIter);
         } else {
             dIter++;
         }
