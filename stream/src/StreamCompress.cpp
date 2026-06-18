0e56371c8644eaf924cdec34ab0ef63a7e73d7f9
Shijie Lin <shijlin@qti.qualcomm.com>
2026-06-18 09:55:06 +0800

    PAL: Change copyright in StreamCompress&PCM
    Update StreamCompress&PCM copyright to
    comply with standards.
    
    Change-Id: I88ab3ab3421cfe3b9c4993b73316ee394027e6e8
    (cherry picked from commit 4b11e708fcccdaef58e22afc43c6dc27664d7751)

--- stream/src/StreamCompress.cpp
+++ stream/src/StreamCompress.cpp
@@ -29,7 +29,7 @@
  * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "PAL: StreamCompress"
